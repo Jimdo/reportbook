bde4fcc951e8776aad12e33ce2750867c24b07d3
Hauke Stange <hauke.stange@jimdo.com>
2016-08-18 18:28:19 +0200

    Delete test artefacts recursively
    It was about my honour! So here it is: traverse a directory tree
    recursively and delete all the files!

--- tests/ReportFileRepositoryTest.php
+++ tests/ReportFileRepositoryTest.php
@@ -10,12 +10,12 @@ class ReportFileRepositoryTest extends TestCase
 
     protected function setUp()
     {
-        $this->removeAllReports();
+        $this->deleteRecursive(self::REPORTS_ROOT_PATH);
     }
 
     protected function tearDown()
     {
-        $this->removeAllReports();
+        $this->deleteRecursive(self::REPORTS_ROOT_PATH);
     }
 
     /**
@@ -95,31 +95,28 @@ class ReportFileRepositoryTest extends TestCase
         $this->assertCount(3, $foundReports);
     }
 
-    private function removeAllReports()
+    private function deleteRecursive($input)
     {
-        if (!file_exists(self::REPORTS_ROOT_PATH)) {
+        if (is_file($input)) {
+            unlink($input);
             return;
         }
 
-        $files = scandir(self::REPORTS_ROOT_PATH);
-
-        foreach ($files as $file) {
-            if ($file === '.' || $file === '..') {
-                continue;
-            }
-
-            $dir = self::REPORTS_ROOT_PATH . '/' . $file;
-            if (is_dir($dir)) {
-                $dirFiles = scandir($dir);
-                foreach ($dirFiles as $file2) {
-                    if ($file2 === '.' || $file2 === '..') {
-                        continue;
-                    }
-
-                    $rmFile = $dir . '/' . $file2;
-                    unlink($rmFile);
+        if (is_dir($input)) {
+            foreach (scandir($input) as $file) {
+                if ($file === '.' || $file === '..') {
+                    continue;
                 }
-                rmdir($dir);
+
+                $file = join('/', [$input, $file]);
+                if (is_file($file)) {
+                    unlink($file);
+                    continue;
+                }
+
+                $this->deleteRecursive($file);
+
+                rmdir($file);
             }
         }
     }

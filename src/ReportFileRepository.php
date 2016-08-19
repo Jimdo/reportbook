ce33b813c2b584313590075331778d6ddf0b43eb
Tom Stich <tom.stich@jimdo.com>
2016-08-19 12:17:35 +0200

    Implementation of File System Repository (#8)
    * Add create to repo
    
    * Add delete() to repo
    
    * Add findAll() to repo
    
    * Fix test
    
    * Delete test artefacts recursively
    
    It was about my honour! So here it is: traverse a directory tree
    recursively and delete all the files!
    
    * Add findByTraineeId() to repo
    
    * Add findByStatus() to repo
    
    * Add findById() to repo

---
+++ src/ReportFileRepository.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Jimdo\Reports;
+
+class ReportFileRepository implements ReportRepository
+{
+    /** @var string */
+    private $reportsPath;
+
+    /**
+     * @param string $reportsPath
+     */
+    public function __construct(string $reportsPath)
+    {
+         $this->reportsPath = $reportsPath;
+    }
+
+    /**
+     * @return string
+     */
+    public function reportsPath(): string
+    {
+        return $this->reportsPath;
+    }
+
+    /**
+     * @param string $traineeId
+     * @param string $content
+     * @return Report
+     */
+    public function create(string $traineeId, string $content): Report
+    {
+        $report = new Report($traineeId, $content);
+        $this->save($report);
+
+        return $report;
+    }
+
+    /**
+     * @param Report $report
+     */
+    public function delete(Report $report)
+    {
+        unlink($this->filename($report));
+    }
+
+    /**
+     * @param Report $report
+     */
+    public function save(Report $report)
+    {
+        $this->ensureReportsPath();
+        $this->ensureTraineeReportsPath($report->traineeId());
+        file_put_contents($this->filename($report), serialize($report));
+    }
+
+    /**
+     * @return Report[]
+     */
+    public function findAll(): array
+    {
+        $foundReports = [];
+        $files = scandir($this->reportsPath);
+        foreach ($files as $traineeId) {
+            if ($traineeId === '.' || $traineeId === '..') {
+                continue;
+            }
+            $reports = scandir($this->reportsPath . '/' . $traineeId);
+            foreach ($reports as $report) {
+                if ($report === '.' || $report === '..') {
+                    continue;
+                }
+                $serializedReport = file_get_contents($this->reportsPath . '/' . $traineeId . '/' . $report);
+                $foundReports[] = unserialize($serializedReport);
+            }
+        }
+        return $foundReports;
+    }
+
+    /**
+     * @param string $traineeId
+     * @return Report[]
+     */
+    public function findByTraineeId(string $traineeId): array
+    {
+        $foundReports = [];
+        $traineePath = $this->reportsPath . '/' . $traineeId;
+        $files = scandir($traineePath);
+        foreach ($files as $reports) {
+            if ($reports === '.' || $reports === '..') {
+                continue;
+            }
+            $serializedReport = file_get_contents($traineePath . '/' . $reports);
+            $foundReports[] = unserialize($serializedReport);
+        }
+        return $foundReports;
+    }
+
+    /**
+     * @param string $status
+     * @return array
+     */
+    public function findByStatus(string $status): array
+    {
+        $allReports = $this->findAll();
+        $foundReports = [];
+
+        foreach ($allReports as $report) {
+            if ($report->status() === $status) {
+                $foundReports[] = $report;
+            }
+        }
+        return $foundReports;
+    }
+
+    /**
+     * @param string $id
+     * @return Report
+     */
+    public function findById(string $id): Report
+    {
+        $allReports = $this->findAll();
+
+        foreach ($allReports as $report) {
+            if ($report->id() === $id) {
+                $foundReport = $report;
+            }
+        }
+        return $foundReport;
+    }
+
+    private function ensureReportsPath()
+    {
+        if (!file_exists($this->reportsPath)) {
+            mkdir($this->reportsPath);
+        }
+    }
+
+    /**
+     * @param string $traineeId
+     */
+    private function ensureTraineeReportsPath(string $traineeId)
+    {
+        $traineeReportsPath = $this->reportsPath . '/' . $traineeId;
+        if (!file_exists($traineeReportsPath)) {
+            mkdir($traineeReportsPath);
+        }
+    }
+
+    /**
+     * @param Report $report
+     * @return string
+     */
+    private function filename(Report $report): string
+    {
+        return $filename = $this->reportsPath . '/' . $report->traineeId() . '/' . $report->id();
+    }
+}
---
+++ tests/ReportFileRepositoryTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Jimdo\Reports;
+
+use PHPUnit\Framework\TestCase;
+
+class ReportFileRepositoryTest extends TestCase
+{
+    const REPORTS_ROOT_PATH = 'tests/reports';
+
+    protected function setUp()
+    {
+        $this->deleteRecursive(self::REPORTS_ROOT_PATH);
+    }
+
+    protected function tearDown()
+    {
+        $this->deleteRecursive(self::REPORTS_ROOT_PATH);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveRepositoryRoot()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+
+        $this->assertEquals(self::REPORTS_ROOT_PATH, $repository->reportsPath());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldCreateReport()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+        $traineeId = uniqid();
+        $expectedContent = 'some content';
+
+        $expectedReport = $repository->create($traineeId, $expectedContent);
+        $reportId = $expectedReport->id();
+
+        $reportFileName = sprintf('%s/%s/%s'
+            , self::REPORTS_ROOT_PATH
+            , $traineeId
+            , $expectedReport->id()
+        );
+
+        $report = unserialize(file_get_contents($reportFileName));
+
+        $this->assertEquals($expectedReport->content(), $report->content());
+    }
+
+    /**
+    * @test
+    */
+    public function itShouldDeleteReport()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+        $traineeId = uniqid();
+        $content = 'some content';
+
+        $report = $repository->create($traineeId, $content);
+
+        $reportFileName = sprintf('%s/%s/%s'
+            , self::REPORTS_ROOT_PATH
+            , $traineeId
+            , $report->id()
+        );
+
+        $this->assertTrue(file_exists($reportFileName));
+
+        $repository->delete($report);
+
+        $this->assertFalse(file_exists($reportFileName));
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFindAllReports()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+        $content = 'some content';
+
+        $this->assertCount(0, $repository->findAll());
+
+        $reports = [];
+        $reports[] = $repository->create(uniqid(), $content);
+        $reports[] = $repository->create(uniqid(), $content);
+        $reports[] = $repository->create(uniqid(), $content);
+
+        $foundReports = $repository->findAll();
+
+        $this->assertCount(3, $foundReports);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFindByTraineeId()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+        $content = 'some content';
+
+        $traineeId1 = uniqid();
+        $traineeId2 = uniqid();
+
+        $reports = [];
+        $reports[] = $repository->create($traineeId1, $content);
+        $reports[] = $repository->create($traineeId1, $content);
+        $reports[] = $repository->create($traineeId2, $content);
+        $reports[] = $repository->create($traineeId2, $content);
+
+        $foundReports = $repository->findByTraineeId($traineeId1);
+
+        $this->assertCount(2, $foundReports);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFindByStatus()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+        $content = 'some content';
+
+        $reports = [];
+        $reports[] = $repository->create(uniqid(), $content);
+        $reports[] = $repository->create(uniqid(), $content);
+        $reports[] = $repository->create(uniqid(), $content);
+
+        $foundReports = $repository->findByStatus(Report::STATUS_NEW);
+
+        $this->assertEquals(Report::STATUS_NEW, $foundReports[0]->status());
+        $this->assertEquals(Report::STATUS_NEW, $foundReports[1]->status());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFindById()
+    {
+        $repository = new ReportFileRepository(self::REPORTS_ROOT_PATH);
+        $content = 'some content';
+
+        $report1 = $repository->create(uniqid(), $content);
+        $report2 = $repository->create(uniqid(), $content);
+
+        $foundReport = $repository->findById($report1->id());
+        $this->assertEquals($report1, $foundReport);
+    }
+
+    private function deleteRecursive($input)
+    {
+        if (is_file($input)) {
+            unlink($input);
+            return;
+        }
+
+        if (is_dir($input)) {
+            foreach (scandir($input) as $file) {
+                if ($file === '.' || $file === '..') {
+                    continue;
+                }
+
+                $file = join('/', [$input, $file]);
+                if (is_file($file)) {
+                    unlink($file);
+                    continue;
+                }
+
+                $this->deleteRecursive($file);
+
+                rmdir($file);
+            }
+        }
+    }
+}

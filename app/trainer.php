3df044a3a248750ae294a12e060aafb1b8c70130
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-08-23 09:52:33 +0200

    Add trainer overview

---
+++ app/trainer.php
@@ -0,0 +1,15 @@
+<?php
+namespace Jimdo\Reports;
+
+require 'bootstrap.php';
+
+require 'views/html_start.html';
+
+require 'views/header_user_trainer.html';
+
+require_once '../vendor/autoload.php';
+
+?>
+
+
+<?php require 'views/html_end.html'; ?>

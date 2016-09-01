bdb2d55ca140b4457dd3c85755e3aa409ac305d4
Hauke Stange <hauke.stange@jimdo.com>
2016-09-01 11:31:24 +0200

    Fix findAll

--- src/ReportFileRepository.php
+++ src/ReportFileRepository.php
@@ -72,6 +72,7 @@ class ReportFileRepository implements ReportRepository
     public function findAll(): array
     {
         $foundReports = [];
+        $this->ensureReportsPath();
         foreach ($this->readDirectory($this->reportsPath) as $traineeId) {
             if ($traineeId === '.' || $traineeId === '..' || $traineeId === '.DS_Store') {
                 continue;

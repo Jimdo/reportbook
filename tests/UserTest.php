232786cd9a1f4c0ae72c3a27fa69132228e7011d
Hauke Stange <hauke.stange@jimdo.com>
2016-09-01 15:03:10 +0200

    Start User model...

---
+++ tests/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Jimdo\Reports;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldHaveForeAndSurname()
+    {
+        # code...
+    }
+}

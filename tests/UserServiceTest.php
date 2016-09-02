927c28c72548cb809144f1c04fa1701a1de8993a
Tom Stich <tom.stich@jimdo.com>
2016-09-02 16:07:09 +0200

    Add createUser to UserInterface

---
+++ tests/UserServiceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jimdo\Reports;
+
+use PHPUnit\Framework\TestCase;
+
+use Jimdo\Reports\Views\User as ReadOnlyUser;
+
+class UserServiceTest extends TestCase
+{
+    /** @var UserService */
+    private $userService;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+    protected function setUp()
+    {
+        $this->userRepository = new UserRepository();
+        $this->userService = new UserService($this->userRepository);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldCreateUser()
+    {
+        $forename = 'Max';
+        $surname = 'Mustermann';
+        $email = 'max.mustermann@hotmail.de';
+        $role = 'Trainee';
+        $password = '123456789';
+
+        $user = $this->userService->createUser($forename, $surname, $email, $role, $password);
+
+        $this->assertEquals($forename, $user->forename());
+    }
+}

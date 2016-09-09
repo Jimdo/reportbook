6d34f939e4b9148a456a48c0e1118a7ef6e508ef
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-09-09 13:17:38 +0200

    Modify error messages in GUI

--- app/views/Report.php
+++ app/views/Report.php
@@ -1,42 +1,43 @@
 <?php use \Jimdo\Reports\Report as Report; ?>
-
-
-<?php if (is_array($this->errorMessages)):
-foreach ($this->errorMessages as $error): ?>
-<ul>
-    <li><?php echo $error; ?></li>
-</ul>
-<?php endforeach;
-endif; ?>
-
 <form action="<?php echo $this->action; ?>" method="POST">
   <fieldset>
-    <legend><?php echo $this->legend; ?></legend>
 
-    <div class="form-group form-group-md col-md-offset-0 col-md-12">
+<div class="row">
+    <legend><?php echo $this->legend; ?></legend>
+</div>
+
+<div class="row">
+    <?php if (is_array($this->errorMessages)):
+    foreach ($this->errorMessages as $error): ?>
+            <div class="alert alert-danger col-md-10 col-md-offset-2" role="alert"><strong><?php echo $error; ?></strong></div>
+    <?php endforeach;
+    endif; ?>
+</div>
+
+    <div class="row">
         <label class="col-md-2 control-label" for="calendarWeek">Kalenderwoche: </label>
         <div class="col-md-2 col-md-offset-0">
             <input class="form-control" type="text" placeholder="Kalenderwoche"
-            <?php echo $this->readonly; ?> id="calendarWeek" name="calendarWeek" value="<?php echo $this->calendarWeek; ?>">
+            <?php echo $this->readonly; ?> id="calendarWeek" name="calendarWeek" value="<?php echo $this->calendarWeek; ?>"></br>
         </div>
     </div>
 
-    <div class="form-group form-group-md col-md-offset-0 col-md-12">
+    <div class="row">
         <label class="col-md-2 control-label" for="date">Datum: </label>
         <div class="col-md-2 col-md-offset-0">
             <input class="form-control" type="text" placeholder="Datum"
-            <?php echo $this->readonly; ?> id="date" name="date" value="<?php echo $this->date; ?>">
+            <?php echo $this->readonly; ?> id="date" name="date" value="<?php echo $this->date; ?>"></br>
         </div>
     </div>
 
-    <div class="form-group form-group-md col-md-offset-0 col-md-12">
+    <div class="row">
         <label class="col-md-2 control-label" for="calendarWeek">Bericht: </label>
         <div class="col-md-10 col-md-offset-0">
-            <textarea <?php echo $this->readonly; ?> id="content" name="content" class="form-control" rows="15"><?php echo $this->content; ?></textarea>
+            <textarea <?php echo $this->readonly; ?> id="content" name="content" class="form-control" rows="15"><?php echo $this->content; ?></textarea></br>
         </div>
     </div>
 
-    <div class="form-group form-group-md col-md-offset-0 col-md-12">
+    <div class="row">
     <?php if ($this->role === 'Trainee'): ?>
         <?php if ($this->status !== Report::STATUS_APPROVED && $this->status !== Report::STATUS_APPROVAL_REQUESTED): ?>
             <input type="hidden" id="reportId" name="reportId" value="<?php echo $this->reportId; ?>" />
@@ -57,7 +58,6 @@ endif; ?>
         <button type="submit" class="btn btn-default col-md-2 col-md-offset-8" style="margin-right: 2px; padding-right: 15px; width: 180px;"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Ablehnen</button>
     </div>
     </form>
-
 
     <form action="/report/approve" method="POST">
     <div>

35b97ca600a3a21a3fccc1a48aa748c9aa15232d
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-09-16 10:46:26 +0200

    Fix cdn in infobar and header

--- app/views/Infobar.php
+++ app/views/Infobar.php
@@ -21,9 +21,6 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
 
-              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-              <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog" aria-hidden="true"></strong></a>
                     <ul class="dropdown-menu">

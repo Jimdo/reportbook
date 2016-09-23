8b42a97ebee521873a2058eca24c60ac3efb28ae
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-09-23 15:27:06 +0200

    Add scripts for ApplicationConfig

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -4,4 +4,6 @@ if [ -z ${MONGO_SERVER_IP+x} ]; then
   export MONGO_SERVER_IP=$(docker-machine ip)
 fi
 
+export APPLICATION_ENV=development
+
 php -S localhost:8000 -t app/ app/router.php
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,4 +4,6 @@ if [ -z ${MONGO_SERVER_IP+x} ]; then
   export MONGO_SERVER_IP=$(docker-machine ip)
 fi
 
+export APPLICATION_ENV=testing
+
 scripts/phpunit

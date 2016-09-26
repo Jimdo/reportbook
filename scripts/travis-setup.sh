4593bbc499664a7b9bcb1e8c0c0529e166187f63
Hauke Stange <hauke.stange@jimdo.com>
2016-09-26 15:36:42 +0200

    debug travis

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+env
+
+echo $PWD
+
 docker run --name reportbook-mongodb \
     -v reportbook-data:/data/db \
-    -v $(TRAVIS_BUILD_DIR)/scripts:/scripts \
+    -v $TRAVIS_BUILD_DIR/scripts:/scripts \
     -p 27017:27017 \
     -d mongo --auth
 

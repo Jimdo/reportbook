ce0df2bd13661748ab9ce91aba176d24bd3c2b8d
Hauke Stange <hauke.stange@jimdo.com>
2016-09-26 15:29:45 +0200

    Move travis docker setup to script

---
+++ scripts/travis-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+docker run --name reportbook-mongodb \
+    -v reportbook-data:/data/db \
+    -v $(TRAVIS_BUILD_DIR)/scripts:/scripts \
+    -p 27017:27017 \
+    -d mongo --auth
+
+docker exec reportbook-mongodb /scripts/setup-mongo-server.sh
+
+docker ps

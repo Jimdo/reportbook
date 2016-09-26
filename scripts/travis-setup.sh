eed9f3be60242cbf20ce8c13829f58f04b3b74eb
Hauke Stange <hauke.stange@jimdo.com>
2016-09-26 15:36:42 +0200

    Setup MongoDB auth and users on travis

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -4,10 +4,12 @@ set -e
 
 docker run --name reportbook-mongodb \
     -v reportbook-data:/data/db \
-    -v $(TRAVIS_BUILD_DIR)/scripts:/scripts \
+    -v $TRAVIS_BUILD_DIR/scripts:/scripts \
     -p 27017:27017 \
     -d mongo --auth
 
-docker exec reportbook-mongodb /scripts/setup-mongo-server.sh
+# let the MongoDB server start up...
+echo "Waiting 2 seconds for the MongoDB server..."
+sleep 2
 
-docker ps
+docker exec reportbook-mongodb /scripts/setup-mongo-server.sh

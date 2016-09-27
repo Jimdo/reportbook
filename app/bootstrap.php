4bf979a93a7d05523b7780a4d071a504307c318c
Hauke Stange <hauke.stange@posteo.de>
2016-09-27 15:45:12 +0200

    Use MongoDB as session store (#87)
    * Fix invocation of mongo client
    
    * Add mongo session handler to composer
    
    Along with the mongo session handler add Monolog as logger.
    
    * Use mongo db as session store
    
    * Fix dev server env

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,28 @@
 <?php
 
-session_start(['save_path' => '/tmp']);
-
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use Jimdo\Reports\Web\ApplicationConfig;
+use Monolog\Logger;
+use Altmetric\MongoSessionHandler;
+
+$applicationConfig = new ApplicationConfig(realpath(__DIR__ . '/../config.yml'));
+
+$uri = sprintf('mongodb://%s:%s@%s:%d/%s'
+    , $applicationConfig->mongoUsername
+    , $applicationConfig->mongoPassword
+    , $applicationConfig->mongoHost
+    , $applicationConfig->mongoPort
+    , $applicationConfig->mongoDatabase
+);
+
+$client = new \MongoDB\Client($uri);
+$db = $client->selectDatabase($applicationConfig->mongoDatabase);
+
+$handler = new MongoSessionHandler($db->sessions, new Logger('session'));
+
+session_set_save_handler($handler);
+
+session_start();
 
 date_default_timezone_set("UTC");

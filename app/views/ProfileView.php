6c4c65ad9a7d198778d64b0d9e7d8b338a7e07a2
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-10-18 14:20:11 +0200

    Change view style

--- app/views/ProfileView.php
+++ app/views/ProfileView.php
@@ -20,8 +20,7 @@
 
              <div class="collapse" id="changeForename">
                  <div class="card card-block" >
-                     <label class="col-sm-6 control-label">Neuer Vorname</label>
-                     <div class="col-sm-6">
+                     <div class="col-sm-offset-6 col-sm-6">
                          <input type="text" name="newForename" class="form-control" id="newForename" placeholder="Neuer Vorname"></br>
                          <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
                      </div>
@@ -42,8 +41,7 @@
 
              <div class="collapse" id="changeSurname">
                  <div class="card card-block" >
-                     <label class="col-sm-6 control-label">Neuer Nachname</label>
-                     <div class="col-sm-6">
+                     <div class="col-sm-offset-6 col-sm-6">
                          <input type="text" name="newSurname" class="form-control" id="newSurname" placeholder="Neuer Nachname"></br>
                          <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
                      </div>
@@ -64,8 +62,7 @@
 
              <div class="collapse" id="changeUsername">
                  <div class="card card-block" >
-                     <label class="col-sm-6 control-label">Neuer Benuztername</label>
-                     <div class="col-sm-6">
+                     <div class="col-sm-offset-6 col-sm-6">
                          <input type="text" name="newUsername" class="form-control" id="newUsername" placeholder="Neuer Benutzername"></br>
                          <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
                      </div>
@@ -86,8 +83,7 @@
 
              <div class="collapse" id="changeEmail">
                  <div class="card card-block" >
-                     <label class="col-sm-6 control-label">Neue E-Mail</label>
-                     <div class="col-sm-6">
+                     <div class="col-sm-offset-6 col-sm-6">
                          <input type="text" name="newEmail" class="form-control" id="newEmail" placeholder="Neue E-Mail"></br>
                          <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
                      </div>
@@ -98,7 +94,7 @@
 
     </form>
 <div class="col-sm-offset-6 col-sm-5">
-    <button class="btn btn-sm btn-default btn-block" type="submit">Passwort ändern</button>
+    <button class="btn btn-md btn-default btn-block" type="submit">Passwort ändern</button>
 
 
 </div>

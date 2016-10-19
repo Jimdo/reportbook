f0c4c1c3739cc14ef8dfb42eb0cbc82da9378d17
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-10-19 10:45:47 +0200

    Add profile data to profile view

--- app/views/ProfileView.php
+++ app/views/ProfileView.php
@@ -52,6 +52,27 @@
 
         <div class="form-group">
 
+            <label class="col-sm-6 control-label">Geburtsdatum</label>
+            <div class="col-sm-6">
+
+                <p class="form-control-static">31.10.1995
+                    <a href="#changeDateOfBirth" data-toggle="collapse" class="glyphicon glyphicon-pencil"></a>
+                </p>
+             </div>
+
+             <div class="collapse" id="changeDateOfBirth">
+                 <div class="card card-block" >
+                     <div class="col-sm-offset-6 col-sm-6">
+                         <input type="text" name="newDateOfBirth" class="form-control" id="newDateOfBirth" placeholder="Neues Geburtsdatum"></br>
+                         <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
+                     </div>
+                 </div>
+             </div>
+
+        </div>
+
+        <div class="form-group">
+
             <label class="col-sm-6 control-label">Benutzername</label>
             <div class="col-sm-6">
 
@@ -93,9 +114,132 @@
         </div>
 
     </form>
-<div class="col-sm-offset-6 col-sm-5">
-    <button class="btn btn-md btn-default btn-block" type="submit">Passwort ändern</button>
 
+    <div class="col-sm-offset-6 col-sm-5">
+        <button class="btn btn-md btn-default btn-block" type="submit">Passwort ändern</button><br>
+    </div>
 
 </div>
+
+<div class="col-sm-offset-2 col-sm-5">
+
+    <div class="col-sm-offset-1 col-sm-10">
+        <h4>Betriebliche Daten</h4><br>
+    </div>
+
+    <form action="/user/editPassword" method="POST" class="form-horizontal">
+
+        <div class="form-group">
+
+            <label class="col-sm-6 control-label">Firma</label>
+            <div class="col-sm-6">
+
+                <p class="form-control-static">Jimdo GmbH
+                    <a href="#changeCompany" data-toggle="collapse" class="glyphicon glyphicon-pencil"></a>
+                </p>
+             </div>
+
+             <div class="collapse" id="changeCompany">
+                 <div class="card card-block" >
+                     <div class="col-sm-offset-6 col-sm-6">
+                         <input type="text" name="newCompany" class="form-control" id="newCompany" placeholder="Neue Firma"></br>
+                         <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
+                     </div>
+                 </div>
+             </div>
+
+        </div>
+
+        <div class="form-group">
+
+            <label class="col-sm-6 control-label">Berufsbezeichung</label>
+            <div class="col-sm-6">
+
+                <p class="form-control-static">Fachinformatiker Anwendungsentwicklung
+                    <a href="#changeJobTitle" data-toggle="collapse" class="glyphicon glyphicon-pencil"></a>
+                </p>
+             </div>
+
+             <div class="collapse" id="changeJobTitle">
+                 <div class="card card-block" >
+                     <div class="col-sm-offset-6 col-sm-6">
+                         <input type="text" name="newJobTitle" class="form-control" id="newJobTitle" placeholder="Neue Berufsbezeichung"></br>
+                         <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
+                     </div>
+                 </div>
+             </div>
+
+        </div>
+    </form>
+</div>
+
+<div class="col-sm-offset-2 col-sm-5">
+    <div class="col-sm-offset-1 col-sm-10">
+        <h4>Schulische Daten</h4><br>
+    </div>
+    <form action="/user/editPassword" method="POST" class="form-horizontal">
+
+        <div class="form-group">
+
+            <label class="col-sm-6 control-label">Schule</label>
+            <div class="col-sm-6">
+
+                <p class="form-control-static">Itech
+                    <a href="#changeSchool" data-toggle="collapse" class="glyphicon glyphicon-pencil"></a>
+                </p>
+             </div>
+
+             <div class="collapse" id="changeSchool">
+                 <div class="card card-block" >
+                     <div class="col-sm-offset-6 col-sm-6">
+                         <input type="text" name="newSchool" class="form-control" id="newSchool" placeholder="Neue Schule"></br>
+                         <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
+                     </div>
+                 </div>
+             </div>
+
+        </div>
+
+        <div class="form-group">
+
+            <label class="col-sm-6 control-label">Klasse</label>
+            <div class="col-sm-6">
+
+                <p class="form-control-static">It6ec
+                    <a href="#changeGrade" data-toggle="collapse" class="glyphicon glyphicon-pencil"></a>
+                </p>
+             </div>
+
+             <div class="collapse" id="changeGrade">
+                 <div class="card card-block" >
+                     <div class="col-sm-offset-6 col-sm-6">
+                         <input type="text" name="newGrade" class="form-control" id="newGrade" placeholder="Neue Klasse"></br>
+                         <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
+                     </div>
+                 </div>
+             </div>
+
+        </div>
+
+        <div class="form-group">
+
+            <label class="col-sm-6 control-label">Ausbildungsjahr</label>
+            <div class="col-sm-6">
+
+                <p class="form-control-static">1
+                    <a href="#changeTrainingYear" data-toggle="collapse" class="glyphicon glyphicon-pencil"></a>
+                </p>
+             </div>
+
+             <div class="collapse" id="changeTrainingYear">
+                 <div class="card card-block" >
+                     <div class="col-sm-offset-6 col-sm-6">
+                         <input type="text" name="newTrainingYear" class="form-control" id="newTrainingYear" placeholder="Neues Ausbildungsjahr"></br>
+                         <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
+                     </div>
+                 </div>
+             </div>
+
+        </div>
+    </form>
 </div>

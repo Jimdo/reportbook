509268b81052eec7ae6fd4e03db8e4543c5f4bc0
Tom Stich <tom.stich@jimdo.com>
2016-10-21 15:45:47 +0200

    Add uploadAction to UserController

--- src/Web/Controller/UserController.php
+++ src/Web/Controller/UserController.php
@@ -55,70 +55,14 @@ class UserController extends Controller
         $this->viewHelper = new ViewHelper();
     }
 
-
-
     public function uploadAction()
     {
-        $target_dir = "app/uploads/";
-        echo basename($_FILES["fileToUpload"]["name"]);
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
-            }
-        }
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
-            $uploadOk = 0;
-        }
-        // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 2000000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
-        }
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
-        }
+        $image = file_get_contents($_FILES["fileToUpload"]["tmp_name"]);
+        $base64 = base64_encode($image);
 
-        $this->service->saveImage($target_file);
-
-        // retrieve
-        // echo $gridFS->findOne(array("_id" => $id))->getBytes();
+        $this->service->editImage($this->sessionData('userId'), $base64);
+        $this->redirect('/user/profile');
     }
-
-
-
-
-
-
-
-
-
-
-
 
     public function indexAction()
     {
@@ -297,7 +241,6 @@ class UserController extends Controller
 
         $profileView = $this->view('app/views/ProfileView.php');
         $profileView->user = $this->service->findUserById($this->sessionData('userId'));
-
         $footerView = $this->view('app/views/Footer.php');
 
         $this->response->addBody($headerView->render());

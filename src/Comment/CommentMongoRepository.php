48cd73306cc2e368c58ed6b625eefa106e078774
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-01 16:03:22 +0100

    Refactor ApplicationConfig in MongoRepositories

--- src/Comment/CommentMongoRepository.php
+++ src/Comment/CommentMongoRepository.php
@@ -28,7 +28,7 @@ class CommentMongoRepository implements CommentRepository
      */
     public function __construct(\MongoDB\Client $client, Serializer $serializer, ApplicationConfig $applicationConfig)
     {
-        $this->applicationConfig = new ApplicationConfig(__DIR__ . '/../../config.yml');
+        $this->applicationConfig = $applicationConfig;
         $this->serializer = $serializer;
         $this->client = $client;
         $this->reportbook = $this->client->selectDatabase($this->applicationConfig->mongoDatabase);

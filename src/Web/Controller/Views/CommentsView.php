f9eb949cb69712393da1703d0671fe1147645ed6
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-02 14:36:38 +0100

    Beautify edit comment

--- src/Web/Controller/Views/CommentsView.php
+++ src/Web/Controller/Views/CommentsView.php
@@ -1,6 +1,7 @@
 <div class="form-horizontal">
 
-<?php foreach ($this->comments as $comment): ?>
+<?php foreach ($this->comments as $comment):
+$editActive = false;?>
 
     <div class="row">
         <div class="col-sm-offset-2 col-sm-1">
@@ -24,6 +25,7 @@
                                 <input type="hidden" name="userId" value="<?php echo $this->userId; ?>">
                                 <input type="hidden" name="commentId" value="<?php echo $comment->id(); ?>">
                                 <button type="submit" class="btn-link glyphicon glyphicon-trash" onclick="return confirm('Soll der Bericht wirklich gelöscht werden?')" aria-hidden="true"></button>
+
                             </form>
                         </div>
 
@@ -40,16 +42,21 @@
 
                     <div class="collapse" id="changeComment<?php echo $comment->id(); ?>">
                         <form action="/comment/editComment" method="POST" class="form-horizontal">
+
                             <div class="card card-block">
-                                <div class="col-sm-offset-6 col-sm-6">
                                     <input type="hidden" name="reportId" value="<?php echo $this->reportId; ?>">
                                     <input type="hidden" name="userId" value="<?php echo $this->userId; ?>">
                                     <input type="hidden" name="commentId" value="<?php echo $comment->id(); ?>">
                                     <input type="hidden" id="traineeId" name="traineeId" value="<?php echo $this->traineeId; ?>">
-                                    <input type="text" name="newComment" class="form-control" id="newComment" placeholder="Neuer Inhalt"></br>
+                                    <textarea rows="5" name="newComment" class="form-control"><?php echo nl2br($comment->content());?></textarea><br>
+                            </div>
+
+                            <div class="form-group">
+                                <div  class="col-sm-offset-10 col-sm-2 ">
                                     <button class="btn btn-md btn-default btn-block" type="submit">Speichern</button>
                                 </div>
                             </div>
+
                         </form>
                     </div>
 

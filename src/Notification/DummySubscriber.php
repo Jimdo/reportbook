fd865f982820b4f3950d8321c136521222b229aa
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-04 15:51:42 +0100

    Implement Subscriber for Notification (#163)
    * Add Subscriber Interface
    
    * Add DummySubscriber and construct
    
    * Add isResponsibleFor to DummySubscriberTest
    
    * Add notify to DummySubscriber

---
+++ src/Notification/DummySubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Jimdo\Reports\Notification;
+
+class DummySubscriber implements Subscriber
+{
+    /** @var array */
+    public $validEventTypes = [];
+
+    /** @var bool */
+    public $notified = false;
+
+    /**
+     * @param array $eventTypes
+     */
+    public function __construct(array $eventTypes)
+    {
+        $this->validEventTypes = $eventTypes;
+    }
+
+    /**
+     * @param Event $event
+     * @return bool
+     */
+    public function isResponsibleFor(Event $event): bool
+    {
+        return in_array($event->type(), $this->validEventTypes);
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function notify(Event $event)
+    {
+        switch ($event->type()) {
+            case 'commentCreated':
+                $this->notified = true;
+                break;
+        }
+    }
+}
---
+++ tests/Notification/DummySubscriberTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Jimdo\Reports\Notification;
+
+use PHPUnit\Framework\TestCase;
+use Jimdo\Reports\Notification\CommentCreated as CommentCreated;
+use Jimdo\Reports\Reportbook\Comment as Comment;
+
+class DummySubscriberTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldHaveValidEventTypes()
+    {
+        $eventTypes = [
+            'dummyType'
+        ];
+
+        $dummySubscriber = new DummySubscriber($eventTypes);
+
+        $this->assertEquals($eventTypes, $dummySubscriber->validEventTypes);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveIsResponsibleFor()
+    {
+        $eventTypes = [
+            'dummyType'
+        ];
+
+        $userId = uniqid();
+        $comment = new Comment(uniqid(), uniqid(), $userId, '11.11.11', 'Some content');
+        $event = new CommentCreated($comment);
+
+        $dummySubscriber = new DummySubscriber($eventTypes);
+        $this->assertFalse($dummySubscriber->isResponsibleFor($event));
+
+        $eventTypes = [
+            'commentCreated'
+        ];
+
+        $dummySubscriber = new DummySubscriber($eventTypes);
+        $this->assertTrue($dummySubscriber->isResponsibleFor($event));
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldNotifyOnRightEventType()
+    {
+        $eventTypes = [
+            'commentCreated'
+        ];
+
+        $userId = uniqid();
+        $comment = new Comment(uniqid(), uniqid(), $userId, '11.11.11', 'Some content');
+        $event = new CommentCreated($comment);
+
+        $dummySubscriber = new DummySubscriber($eventTypes);
+
+        $dummySubscriber->notify($event);
+
+        $this->assertTrue($dummySubscriber->notified);
+    }
+}

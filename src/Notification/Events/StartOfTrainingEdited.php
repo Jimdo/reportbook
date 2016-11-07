a9b0f9229f77e342e21ee01318eae5d2628e3176
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-07 11:35:58 +0100

    Add StartOfTrainingEdited Event

---
+++ src/Notification/Events/StartOfTrainingEdited.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Jimdo\Reports\Notification\Events;
+
+class StartOfTrainingEdited implements Event
+{
+    /** @var array */
+    private $payload;
+
+    /**
+     * @param array $payload
+     */
+    public function __construct(array $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * @return string
+     */
+    public function type(): string
+    {
+        return 'startofTrainingEdited';
+    }
+
+    /**
+     * @return array
+     */
+    public function payload(): array
+    {
+        return $this->payload;
+    }
+}
---
+++ tests/Notification/Events/StartOfTrainingEditedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jimdo\Reports\Notification\Events;
+
+use PHPUnit\Framework\TestCase;
+
+class StartOfTrainingEditedTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldHaveType()
+    {
+        $payload = [];
+        $event = new StartOfTrainingEdited($payload);
+
+        $this->assertEquals('startofTrainingEdited', $event->type());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHavePayload()
+    {
+        $payload = [
+            'userId' => 'afhoafdo',
+            'calendarWeek' => 'nfsdk',
+            'content' => 'dsbds'
+        ];
+
+        $event = new StartOfTrainingEdited($payload);
+
+        $this->assertEquals($payload, $event->payload());
+    }
+}

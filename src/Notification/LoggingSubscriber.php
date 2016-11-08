022d883ae2c39ec2eca19cab56dca21429c25458
Tom Stich <tom.stich@jimdo.com>
2016-11-08 09:11:34 +0100

    Remove '.' after log text

--- src/Notification/LoggingSubscriber.php
+++ src/Notification/LoggingSubscriber.php
@@ -39,85 +39,85 @@ class LoggingSubscriber implements Subscriber
     {
         switch ($event->type()) {
             case 'reportCreated':
-                $this->writeLog("Report with id {$event->payload()['reportId']} by user {$event->payload()['userId']} created.");
+                $this->writeLog("Report with id {$event->payload()['reportId']} by user {$event->payload()['userId']} created");
                 break;
             case 'reportEdited':
-                $this->writeLog("Report with id {$event->payload()['reportId']} by user {$event->payload()['userId']} edited.");
+                $this->writeLog("Report with id {$event->payload()['reportId']} by user {$event->payload()['userId']} edited");
                 break;
             case 'reportDeleted':
-                $this->writeLog("Report with id {$event->payload()['reportId']} by user {$event->payload()['userId']} deleted.");
+                $this->writeLog("Report with id {$event->payload()['reportId']} by user {$event->payload()['userId']} deleted");
                 break;
             case 'approvalRequested':
-                $this->writeLog("Approval for Report with id {$event->payload()['reportId']} requested by user {$event->payload()['userId']}.");
+                $this->writeLog("Approval for Report with id {$event->payload()['reportId']} requested by user {$event->payload()['userId']}");
                 break;
             case 'commentCreated':
-                $this->writeLog("Comment with id {$event->payload()['reportId']} by user {$event->payload()['userId']} created.");
+                $this->writeLog("Comment with id {$event->payload()['reportId']} by user {$event->payload()['userId']} created");
                 break;
             case 'commentEdited':
-                $this->writeLog("Comment with id {$event->payload()['reportId']} by user {$event->payload()['userId']} edited.");
+                $this->writeLog("Comment with id {$event->payload()['reportId']} by user {$event->payload()['userId']} edited");
                 break;
             case 'commentDeleted':
-                $this->writeLog("Comment with id {$event->payload()['reportId']} by user {$event->payload()['userId']} deleted.");
+                $this->writeLog("Comment with id {$event->payload()['reportId']} by user {$event->payload()['userId']} deleted");
                 break;
             case 'companyEdited':
-                $this->writeLog("Company of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Company of user {$event->payload()['userId']} edited");
                 break;
             case 'dateOfBirthEdited':
-                $this->writeLog("Date of birth of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Date of birth of user {$event->payload()['userId']} edited");
                 break;
             case 'emailEdited':
-                $this->writeLog("Email of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Email of user {$event->payload()['userId']} edited");
                 break;
             case 'forenameEdited':
-                $this->writeLog("Forename of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Forename of user {$event->payload()['userId']} edited");
                 break;
             case 'gradeEdited':
-                $this->writeLog("Grade of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Grade of user {$event->payload()['userId']} edited");
                 break;
             case 'imageEdited':
-                $this->writeLog("Image of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Image of user {$event->payload()['userId']} edited");
                 break;
             case 'jobTitleEdited':
-                $this->writeLog("Job title of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Job title of user {$event->payload()['userId']} edited");
                 break;
             case 'passwordEdited':
-                $this->writeLog("Password of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Password of user {$event->payload()['userId']} edited");
                 break;
             case 'reportApproved':
-                $this->writeLog("Report with id {$event->payload()['reportId']} approved by user {$event->payload()['userId']}.");
+                $this->writeLog("Report with id {$event->payload()['reportId']} approved by user {$event->payload()['userId']}");
                 break;
             case 'reportDisapproved':
-                $this->writeLog("Report with id {$event->payload()['reportId']} disapproved by user {$event->payload()['userId']}.");
+                $this->writeLog("Report with id {$event->payload()['reportId']} disapproved by user {$event->payload()['userId']}");
                 break;
             case 'roleApproved':
-                $this->writeLog("User {$event->payload()['userId']} approved.");
+                $this->writeLog("User {$event->payload()['userId']} approved");
                 break;
             case 'roleDisapproved':
-                $this->writeLog("User {$event->payload()['userId']} disapproved.");
+                $this->writeLog("User {$event->payload()['userId']} disapproved");
                 break;
             case 'schoolEdited':
-                $this->writeLog("School of user {$event->payload()['userId']} edited.");
+                $this->writeLog("School of user {$event->payload()['userId']} edited");
                 break;
             case 'startOfTrainingEdited':
-                $this->writeLog("Start of training of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Start of training of user {$event->payload()['userId']} edited");
                 break;
             case 'surnameEdited':
-                $this->writeLog("Surname of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Surname of user {$event->payload()['userId']} edited");
                 break;
             case 'traineeRegistered':
-                $this->writeLog("Trainee with id {$event->payload()['userId']} registered.");
+                $this->writeLog("Trainee with id {$event->payload()['userId']} registered");
                 break;
             case 'trainerRegistered':
-                $this->writeLog("Trainer with id {$event->payload()['userId']} registered.");
+                $this->writeLog("Trainer with id {$event->payload()['userId']} registered");
                 break;
             case 'trainingYearEdited':
-                $this->writeLog("Training year of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Training year of user {$event->payload()['userId']} edited");
                 break;
             case 'userAuthorized':
-                $this->writeLog("User with id {$event->payload()['userId']} logged in.");
+                $this->writeLog("User with id {$event->payload()['userId']} logged in");
                 break;
             case 'usernameEdited':
-                $this->writeLog("Username of user {$event->payload()['userId']} edited.");
+                $this->writeLog("Username of user {$event->payload()['userId']} edited");
                 break;
         }
     }

2e500a40d186ea83a8c2d437ba24a458fb41d692
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-11 11:51:44 +0100

    Implement MailgunSubscriber (#206)
    * Install composer packages
    
    * Add MailgunSubscriber and Test
    
    * Add sendMail and UserService to MailgunSubscriber

---
+++ src/Notification/MailgunSubscriber.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Jimdo\Reports\Notification;
+
+use Jimdo\Reports\Notification\Events\Event;
+use Jimdo\Reports\Web\ApplicationConfig;
+use Jimdo\Reports\User\UserService;
+use Jimdo\Reports\User\UserMongoRepository;
+use Jimdo\Reports\Serializer;
+use Mailgun\Mailgun;
+
+class MailgunSubscriber implements Subscriber
+{
+    /** @var array */
+    private $validEventTypes;
+
+    /** @var Mailgun */
+    private $mailgunClient;
+
+    /** @var UserService */
+    private $userService;
+
+    /** @var string */
+    private $domain;
+
+    /** @var ApplicationConfig */
+    public $appConfig;
+
+    /**
+     * @param array $eventTypes
+     * @param ApplicationConfig $appConfig
+     */
+    public function __construct(array $eventTypes, ApplicationConfig $appConfig)
+    {
+        $this->validEventTypes = $eventTypes;
+        $this->appConfig = $appConfig;
+
+        $this->mailgunClient = new Mailgun('key-1beb90fc6968263d6ba2cf95fc6ca8bb');
+        $this->domain = "sandboxd9e5c670536f4828a8ded4df88519471.mailgun.org";
+
+        $uri = sprintf('mongodb://%s:%s@%s:%d/%s'
+            , $appConfig->mongoUsername
+            , $appConfig->mongoPassword
+            , $appConfig->mongoHost
+            , $appConfig->mongoPort
+            , $appConfig->mongoDatabase
+        );
+
+        $client = new \MongoDB\Client($uri);
+
+        $userRepository = new UserMongoRepository($client, new Serializer(), $appConfig);
+        $this->userService = new UserService($userRepository, $appConfig, new NotificationService());
+    }
+
+    /**
+     * @param Event $event
+     * @return bool
+     */
+    public function isResponsibleFor(Event $event): bool
+    {
+        return in_array($event->type(), $this->validEventTypes);
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function notify(Event $event)
+    {
+
+    }
+
+    protected function sendMail(string $emailTo, string $emailSubject, string $emailText)
+    {
+        $result = $this->mailgunClient->sendMessage("$this->domain", [
+            'from'    => 'Online Berichtsheft <postmaster@sandboxd9e5c670536f4828a8ded4df88519471.mailgun.org>',
+            'to'      => $emailTo,
+            'subject' => $emailSubject,
+            'text'    => $emailText
+        ]);
+    }
+}

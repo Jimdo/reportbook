785f771028bca3f1bdc7881b803ff6aacd721806
Tom Stich <tom.stich@jimdo.com>
2016-11-15 16:33:30 +0100

    Modify benchmark

--- benchmarks/Reportbook/ReportMongoRepositoryBench.php
+++ benchmarks/Reportbook/ReportMongoRepositoryBench.php
@@ -7,25 +7,41 @@ use Jimdo\Reports\Serializer as Serializer;
 
 class ReportMongoRepositoryBench
 {
+    /** @var Client $client */
+    private $client;
+
+    /** @var Collection $users */
+    private $reports;
+
+    /** @var ApplicationConfig */
+    private $appConfig;
+
+    protected function setUp()
+    {
+        $this->appConfig = new ApplicationConfig(__DIR__ . '/../../config.yml');
+
+        $uri = sprintf('mongodb://%s:%s@%s:%d/%s'
+            , $this->appConfig->mongoUsername
+            , $this->appConfig->mongoPassword
+            , $this->appConfig->mongoHost
+            , $this->appConfig->mongoPort
+            , $this->appConfig->mongoDatabase
+        );
+
+        $this->client = new \MongoDB\Client($uri);
+
+        $reportbook = $this->client->selectDatabase($this->appConfig->mongoDatabase);
+        $this->reports = $reportbook->reports;
+
+
+    }
+
     /**
      * @Revs(10)
      */
     public function benchFindAll()
     {
-        $appConfig = new ApplicationConfig(__DIR__ . '/../../config.yml');
-
-        $uri = sprintf('mongodb://%s:%s@%s:%d/%s'
-            , $appConfig->mongoUsername
-            , $appConfig->mongoPassword
-            , $appConfig->mongoHost
-            , $appConfig->mongoPort
-            , $appConfig->mongoDatabase
-        );
-
-        $client = new \MongoDB\Client($uri);
-
-        $repository = new ReportMongoRepository($client, new Serializer(), $appConfig);
-
-        $repository->findAll();
+        $this->setUp();
+        $this->reports->find();
     }
 }

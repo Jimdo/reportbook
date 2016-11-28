9cf8af458443cd96e4f732b18b0dbf5f8f8494b0
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-28 12:08:48 +0100

    Add class PasswordLength with correct check

---
+++ src/User/PasswordConstraints/PasswordLength.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jimdo\Reports\User\PasswordConstraints;
+
+class PasswordLength extends PasswordConstraintsFactory
+{
+    const PASSWORD_LENGTH = 7;
+
+    /**
+     * @return bool
+     */
+    public function check(string $password): bool
+    {
+        if (strlen($password) < self::PASSWORD_LENGTH) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}

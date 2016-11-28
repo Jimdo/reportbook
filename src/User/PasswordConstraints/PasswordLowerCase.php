6595ffd9f2a5b1ef49a275cb41e70a376ad6cb47
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-11-28 13:27:49 +0100

    Add PasswordLowerCase and check

---
+++ src/User/PasswordConstraints/PasswordLowerCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jimdo\Reports\User\PasswordConstraints;
+
+class PasswordLowerCase extends PasswordConstraintsFactory
+{
+    /**
+     * @param string $password
+     * @return bool
+     */
+    public function check(string $password): bool
+    {
+        for ($i=0; $i < strlen($password); $i++) {
+            if (ctype_lower($password[$i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

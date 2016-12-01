c6c6a01c329bb39825c69108dd2441f2e694aa9c
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-12-01 10:13:39 +0100

    Create collections for migration

--- scripts/mongo-migration.sh
+++ scripts/mongo-migration.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+# Create collections
+mongo $MONGO_PROD_HOST \
+  -u $MONGO_PROD_USERNAME \
+  -p $MONGO_PROD_PASSWORD \
+  < scripts/mongo/create-collections.js
+
 # Create unique indices
-mongo $MONGO_PROD_HOST -u $MONGO_PROD_USERNAME -p $MONGO_PROD_PASSWORD < scripts/mongo/create-indices.js
+mongo $MONGO_PROD_HOST \
+  -u $MONGO_PROD_USERNAME \
+  -p $MONGO_PROD_PASSWORD \
+  < scripts/mongo/create-indices.js

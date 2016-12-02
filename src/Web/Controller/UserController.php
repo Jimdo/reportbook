3f9f4f90e6fc8fdda3728817861c9920bb03d7c6
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-12-02 09:45:33 +0100

    Modify login with admin

--- src/Web/Controller/UserController.php
+++ src/Web/Controller/UserController.php
@@ -203,13 +203,12 @@ class UserController extends Controller
         $loginWithAdminDefaultPassword = false;
         if ($identifier === self::ADMIN_DEFAULT_USER && $password === self::ADMIN_DEFAULT_PASSWORD) {
             if (!$this->service->exists($identifier)) {
-                if (!$this->service->checkForTrainer()) {
-                    $adminUser = $this->service->registerTrainer(
+                if (!$this->service->checkForAdmin()) {
+                    $adminUser = $this->service->registerAdmin(
                         self::ADMIN_DEFAULT_USER,
                         'admin',
                         self::ADMIN_DEFAULT_PASSWORD
                     );
-                    $this->service->approveRole($adminUser->email());
                     $this->profileService->createProfile($adminUser->id(), 'admin', 'admin');
                 }
             }

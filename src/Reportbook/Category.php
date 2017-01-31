a3f10819a204d312de6b035f56aa0152875a27b0
Tom Stich <tom.stich@jimdo.com>
2017-01-31 10:31:27 +0100

    Remove tests and use Category as const collection

--- src/Reportbook/Category.php
+++ src/Reportbook/Category.php
@@ -6,23 +6,4 @@ class Category
 {
     const SCHOOL = 'SCHOOL';
     const COMPANY = 'COMPANY';
-
-    /** @var string */
-    private $name;
-
-    /**
-     * @param $type
-     */
-    public function __construct(string $name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return string
-     */
-    public function name(): string
-    {
-        return $this->name;
-    }
 }

78049ea3b0f34e67ff51daaf4739325e122bfd7e
Tom Stich <tom.stich@jimdo.com>
2017-02-07 09:56:34 +0100

    Add showMonths() to ViewHelper

--- src/Web/ViewHelper.php
+++ src/Web/ViewHelper.php
@@ -10,6 +10,80 @@ use Jimdo\Reports\Reportbook\Category;
 class ViewHelper
 {
     /**
+     * @param int $month
+     * @param int $year
+     * @param array $cwInfo
+     */
+    public function showMonth(int $month, int $year, array $cwInfo)
+    {
+        $date = mktime(12, 0, 0, $month, 1, $year);
+        $daysInMonth = date("t", $date);
+
+        $offset = date("w", $date) - 1;
+
+        if ($offset === -1) {
+            $offset = 6;
+        }
+
+        $rows = 1;
+
+        echo "<table class=\"table-condensed table-bordered table-striped table table-curved\">\n";
+        echo "\t<tr><th>Mo</th><th>Di</th><th>Mi</th><th>Do</th><th>Fr</th><th>Sa</th><th>So</th></tr>";
+        echo "\n\t<tr>";
+
+
+        for($i = 1; $i <= $offset; $i++)
+        {
+            echo "<td></td>";
+        }
+
+
+        for($day = 1; $day <= $daysInMonth; $day++) {
+
+            $currentWeek = intVal(date("W", strtotime("$year-$month-$day")));
+
+            if( ($day + $offset - 1) % 7 == 0 && $day != 1) {
+                echo "</tr>\n\t<tr>";
+
+                $rows++;
+            }
+
+            // var_dump($cwInfo[10]);
+
+
+            if ($this->checkIfDayIsInCalendarWeek(
+                $day, $currentWeek, $month, $year) &&
+                array_key_exists($currentWeek, $cwInfo) &&
+                $cwInfo[$currentWeek] !== ''
+            ) {
+
+                switch ($cwInfo[$currentWeek]) {
+                    case Report::STATUS_APPROVAL_REQUESTED:
+                        $color = 'yellow';
+                        break;
+                    case Report::STATUS_APPROVED:
+                        $color = '#01DF01';
+                        break;
+                    case Report::STATUS_DISAPPROVED:
+                    $color = 'red';
+                        break;
+                }
+                echo "<td bgcolor=\"$color\" class=\"text-center\">" . $day .  "</td>";
+            } else {
+                echo "<td class=\"text-center\">" . $day .  "</td>";
+            }
+        }
+
+        while( ($day + $offset) <= $rows * 7) {
+            echo "<td></td>";
+            $day++;
+        }
+
+        echo "</tr>\n";
+        echo "</table>\n";
+    }
+
+    /**
     * @param string $status
     * @return string
     */

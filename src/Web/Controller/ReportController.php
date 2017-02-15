6324b8fd1f173d59fa3b4a098307ddcc276a36d1
Tom Stich <tom.stich@jimdo.com>
2017-02-15 16:13:03 +0100

    Add twig to calendarAction and view

--- src/Web/Controller/ReportController.php
+++ src/Web/Controller/ReportController.php
@@ -213,53 +213,36 @@ class ReportController extends Controller
         if (!$this->isTrainee() && !$this->isAdmin() && !$this->isTrainer()) {
             $this->redirect('/user');
         } else {
-            $headerView = $this->view('src/Web/Controller/Views/Header.php');
-            $headerView->tabTitle = 'Berichtsheft';
-
-            $infobarView = $this->view('src/Web/Controller/Views/Infobar.php');
-            $infobarView->viewHelper = $this->viewHelper;
-            $infobarView->username = $this->sessionData('username');
-            $infobarView->role = $this->sessionData('role');
-            $infobarView->trainerRole = $this->isTrainer();
-            $infobarView->adminRole = $this->isAdmin();
-            $infobarView->infoHeadline = ' | Übersicht';
-            $infobarView->hideInfos = false;
-
-            $calendarView = $this->view('src/Web/Controller/Views/CalendarView.php');
-            $calendarView->viewHelper = $this->viewHelper;
-            $calendarView->trainerRole = $this->isTrainer();
-            $calendarView->adminRole = $this->isAdmin();
-            $calendarView->months = ['Januar', 'Febuar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
-
             $year = $this->queryParams('year');
             if ($year === null) {
                 $year = date('Y');
             }
-            $calendarView->year = $year;
-
             if ($this->isAdmin() || $this->isTrainer()) {
                 $users = $this->appService->findAllTrainees();
-
                 foreach ($users as $user) {
                     $profile = $this->appService->findProfileByUserId($user->id());
                     $traineeInfo[] = ['name' => $profile->forename() . ' ' .  $profile->surname(), 'id' => $user->id()];
                 }
-                $calendarView->users = $traineeInfo;
-                $calendarView->currentUserId = $this->queryParams('userId');
-                $calendarView->cwInfo = $this->createCalendarArray($this->queryParams('userId'), $year);
-            } elseif ($this->isTrainee()) {
-                $user = $this->queryParams('userId');
-                $calendarView->currentUserId = $user;
-                $calendarView->cwInfo = $this->createCalendarArray($user, $year);
             }
 
-            $footerView = $this->view('src/Web/Controller/Views/Footer.php');
-            $footerView->backButton = false;
+            $variables = [
+                'tabTitle' => 'Berichtsheft',
+                'backButton' => false,
+                'viewHelper' => $this->viewHelper,
+                'username' => $this->sessionData('username'),
+                'role' => $this->sessionData('role'),
+                'isTrainer' => $this->isTrainer(),
+                'isAdmin' => $this->isAdmin(),
+                'infoHeadline' => ' | Übersicht',
+                'hideInfos' => false,
+                'months' => ['Januar', 'Febuar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+                'year' => $year,
+                'users' => $traineeInfo,
+                'currentUserId' => $this->queryParams('userId'),
+                'cwInfo' => $this->createCalendarArray($this->queryParams('userId'), $year)
+            ];
 
-            $this->response->addBody($headerView->render());
-            $this->response->addBody($infobarView->render());
-            $this->response->addBody($calendarView->render());
-            $this->response->addBody($footerView->render());
+            echo $this->twig->render('Calendar.html', $variables);
         }
     }
 

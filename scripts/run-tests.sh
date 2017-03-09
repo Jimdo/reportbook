f53fb18502bee97bdf34635db384cee82573d98b
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-09 12:06:47 +0100

    Add MYSQL_HOST to testing env

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,6 +4,17 @@ if [ -z ${MONGO_HOST+x} ]; then
   export MONGO_HOST=$(docker-machine ip)
 fi
 
+if [ -z ${MYSQL_HOST+x} ]; then
+  export MYSQL_HOST=$(docker-machine ip)
+fi
+
 export APPLICATION_ENV=test
 
-scripts/phpunit
+docker run -it --rm \
+    -e APPLICATION_ENV=$APPLICATION_ENV \
+    -e MONGO_HOST=$MONGO_HOST \
+    -e MYSQL_HOST=$MYSQL_HOST \
+    -v $(PWD)/:/var/www/ \
+    --add-host="docker_host:$(docker-machine inspect --format '{{ .Driver.HostOnlyCIDR}}' | awk -F/ '{print $1}')" \
+    jimdo/reportbook:debug \
+    scripts/phpunit

efe2acc631cafdc4091d977ddfe725b86db33cd1
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-09 12:24:47 +0100

    Fix MySQL on Travis

---
+++ scripts/run-tests-travis.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z ${MONGO_HOST+x} ]; then
+  export MONGO_HOST=$(docker-machine ip)
+fi
+
+if [ -z ${MYSQL_HOST+x} ]; then
+  export MYSQL_HOST=$(docker-machine ip)
+fi
+
+export APPLICATION_ENV=test
+
+docker run -it --rm \
+    -e APPLICATION_ENV=$APPLICATION_ENV \
+    -e MONGO_HOST=$MONGO_HOST \
+    -e MYSQL_HOST=$MYSQL_HOST \
+    -v $(PWD)/:/var/www/ \
+    jimdo/reportbook \
+    scripts/phpunit
--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -13,3 +13,11 @@ echo "Waiting 2 seconds for the MongoDB server..."
 sleep 2
 
 docker exec reportbook-mongodb /scripts/setup-mongo-server.sh
+
+docker run --name mysql \
+    -p 3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+    -e MYSQL_DATABASE=$MYSQL_DATABASE \
+    -e MYSQL_USER=$MYSQL_USER \
+    -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+    mysql

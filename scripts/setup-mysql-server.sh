68dc2b42c615b106a68987945c6dd6eba80bf076
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-14 13:32:57 +0100

    Put mysql connection string into variable

--- scripts/setup-mysql-server.sh
+++ scripts/setup-mysql-server.sh
@@ -12,22 +12,30 @@ eval MYSQL_USER=$MYSQL_USER
 eval MYSQL_PASSWORD=$MYSQL_PASSWORD
 eval MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
 
+MYSQL_CMD="mysql -u root"
+
+if [ -n $MYSQL_ROOT_PASSWORD ]; then
+  MYSQL_CMD="mysql -u root -p${MYSQL_ROOT_PASSWORD}"
+fi
+
+echo $MYSQL_CMD
+
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | $MYSQL_CMD
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}dev/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD
+    | mysql -u root -p$MYSQL_ROOT_PASSWORD
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev < scripts/mysql/mysql-dump.sql
+$MYSQL_CMD ${MYSQL_DATABASE}dev < scripts/mysql/mysql-dump.sql
 
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | $MYSQL_CMD
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}test/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD
+    | $MYSQL_CMD
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test < scripts/mysql/mysql-dump.sql
+$MYSQL_CMD ${MYSQL_DATABASE}test < scripts/mysql/mysql-dump.sql

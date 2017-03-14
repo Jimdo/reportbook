576de0a0a2ad391352300c90edc554b2d8b2ba2b
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-14 09:47:38 +0100

    Debug / Fix Travis MySQL setup

--- scripts/setup-mysql-server.sh
+++ scripts/setup-mysql-server.sh
@@ -14,25 +14,25 @@ eval MYSQL_PASSWORD=$MYSQL_PASSWORD
 eval MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
 
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h localhost
 
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}dev/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD
+    | mysql -p$MYSQL_ROOT_PASSWORD -h localhost
 
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev < scripts/mysql/mysql-dump.sql
+ mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev -h localhost < scripts/mysql/mysql-dump.sql
 
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h localhost
 
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}test/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD
+    | mysql -p$MYSQL_ROOT_PASSWORD -h localhost
 
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test < scripts/mysql/mysql-dump.sql
+ mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test -h localhost < scripts/mysql/mysql-dump.sql

1e562d459fd7db18acf6b0de6c93126291ed2d42
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-14 10:35:31 +0100

    Fix mysql setup on travis

--- scripts/setup-mysql-server.sh
+++ scripts/setup-mysql-server.sh
@@ -13,26 +13,30 @@ eval MYSQL_USER=$MYSQL_USER
 eval MYSQL_PASSWORD=$MYSQL_PASSWORD
 eval MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
 
+if [ -z ${MYSQL_PORT+x} ]; then
+  MYSQL_PORT=3306
+fi
+
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
 
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}dev/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
+    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
 
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev -h 127.0.0.1 < scripts/mysql/mysql-dump.sql
+ mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev -h 127.0.0.1 -P$MYSQL_PORT < scripts/mysql/mysql-dump.sql
 
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
 
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}test/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
+    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
 
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test -h 127.0.0.1 < scripts/mysql/mysql-dump.sql
+ mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test -h 127.0.0.1 -P$MYSQL_PORT < scripts/mysql/mysql-dump.sql

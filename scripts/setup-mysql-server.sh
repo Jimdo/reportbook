6ff6d45c56d89547a19ccbcfa9e18311d34fee8e
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-14 11:08:37 +0100

    Remove MySQL port

--- scripts/setup-mysql-server.sh
+++ scripts/setup-mysql-server.sh
@@ -8,37 +8,31 @@ then
     source .env
 fi
 
-if [ -z $MYSQL_PORT ]; then
-  export MYSQL_PORT=3306
-fi
-
 eval MYSQL_DATABASE=$MYSQL_DATABASE
 eval MYSQL_USER=$MYSQL_USER
 eval MYSQL_PASSWORD=$MYSQL_PASSWORD
 eval MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
-eval MYSQL_PORT=$MYSQL_PORT
-
 
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
 
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}dev/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}dev/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
+    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
 
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev -h 127.0.0.1 -P$MYSQL_PORT < scripts/mysql/mysql-dump.sql
+ mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}dev -h 127.0.0.1 < scripts/mysql/mysql-dump.sql
 
 # Create database
-sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
+sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" scripts/mysql/create-database.sql | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
 
 # Create user
 sed "s/MYSQL_USER/${MYSQL_USER}test/g" scripts/mysql/create-user.sql \
     | sed "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" \
     | sed "s/MYSQL_DATABASE/${MYSQL_DATABASE}test/g" \
-    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1 -P$MYSQL_PORT
+    | mysql -p$MYSQL_ROOT_PASSWORD -h 127.0.0.1
 
 # Create tables from database dump
- mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test -h 127.0.0.1 -P$MYSQL_PORT < scripts/mysql/mysql-dump.sql
+ mysql -p$MYSQL_ROOT_PASSWORD ${MYSQL_DATABASE}test -h 127.0.0.1 < scripts/mysql/mysql-dump.sql

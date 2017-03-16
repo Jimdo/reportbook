c82d99654731cf062d49b7d66c14f7b32af9fb8c
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-16 16:12:55 +0100

    Add createComment and save to CommentMySQLRepository

--- src/Reportbook/CommentMySQLRepository.php
+++ src/Reportbook/CommentMySQLRepository.php
@@ -31,4 +31,42 @@ class CommentMySQLRepository implements CommentRepository
         $this->dbHandler = $dbHandler;
         $this->table = 'comment';
     }
+
+    /**
+     * @param string $reportId
+     * @param string $userId
+     * @param string $date
+     * @param string $content
+     * @return Comment
+     */
+    public function createComment(string $reportId, string $userId, string $date, string $content): Comment
+    {
+        $comment = new Comment(uniqId(), $reportId, $userId, $date, $content);
+
+        $this->save($comment);
+
+        return $comment;
+    }
+
+    /**
+     * @param Comment $comment
+     * @throws CommentRepositoryException
+     */
+    public function save(Comment $comment)
+    {
+        $sql = "INSERT INTO $this->table (
+            id, content, date, status, userId, reportId
+        ) VALUES (
+            ?, ?, ?, ?, ?, ?
+        )";
+        $sth = $this->dbHandler->prepare($sql);
+        $sth->execute([
+            $comment->id(),
+            $comment->content(),
+            $comment->date(),
+            $comment->status(),
+            $comment->userId(),
+            $comment->reportId()
+        ]);
+    }
 }

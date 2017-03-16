687353dd10892b17ee924028863bcbd2eae73693
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-16 11:20:53 +0100

    Add MongoSerializer Interface

---
+++ src/MongoSerializer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Jimdo\Reports;
+
+use Jimdo\Reports\User\User;
+use Jimdo\Reports\Reportbook\Comment;
+use Jimdo\Reports\Profile\Profile;
+use Jimdo\Reports\Reportbook\Report;
+
+interface MongoSerializer {
+    /**
+     * @param User $user
+     * @return array
+     */
+    public function serializeUser(User $user): array;
+
+    /**
+     * @param array $serializedUser
+     * @return User
+     */
+    public function unserializeUser(array $serializedUser): User;
+
+    /**
+     * @param User $user
+     * @return array
+     */
+    public function serializeProfile(Profile $profile): array;
+
+    /**
+     * @param array $serializedProfile
+     * @return Profile
+     */
+    public function unserializeProfile(array $serializedProfile): Profile;
+
+    /**
+     * @param Report $report
+     * @return array
+     */
+    public function serializeReport($report): array;
+
+    /**
+     * @param array $serializedReport
+     * @return Report
+     */
+    public function unserializeReport(array $serializedReport): Report;
+
+    /**
+     * @param Comment $comment
+     * @return array
+     */
+    public function serializeComment(Comment $comment): array;
+
+    /**
+     * @param array $serializedComment
+     * @return Comment
+     */
+    public function unserializeComment(array $serializedComment): Comment;
+}

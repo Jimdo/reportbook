6236b30a5bccf32bca06626f7802021d3cd65c0a
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-03-28 11:46:35 +0200

    Add PrintService for creating pdf files of reports  (#405)
    * Add mPDF to composer
    
    * Fix composer require pdf
    
    * Add printer_templates and printer_output paths to config.yml
    
    * Add PrintServiceTest setup
    
    * Add PrintService setup and templates
    
    * Add Tests and Methods to PrintService - PrintServiceTest
    
    * Add pdf files to gitignore
    
    * Setup pdfParser and ensure printerOutput directory exists
    
    * Rename path for test in config.yml
    
    * Add pdfParser to printReports test
    
    * Remove obsolete varible
    
    * Fix linting
    
    * WIP
    
    * Decrease maxLinesPerSite to 18
    
    * Add if statement to Report for translation of category
    
    * Modify content string
    
    * Remove var_dump
    
    * Fix assertion in test
    
    * Add bool to functions for printing the whole reportbook
    
    * Add printReportbook to PrinterService
    
    * Add docs
    
    * Debug travis
    
    * Debug travis
    
    * Change path of Templates for travis
    
    * Fix path for travis
    
    * Update composer
    
    * Remove debug code
    
    * Add space to string
    
    * Remove pdf files
    
    * Remove function realpath
    
    * Fix path for travis
    
    * Make sure directory exists

---
+++ src/Printer/PrintService.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace Jimdo\Reports\Printer;
+
+use Jimdo\Reports\Profile\ProfileService;
+use Jimdo\Reports\Reportbook\ReportbookService;
+use Jimdo\Reports\Web\ApplicationConfig;
+
+class PrintService
+{
+    /** @var mPDF */
+    private $mpdf;
+
+    /** @var ReportbookService */
+    private $reportService;
+
+    /** @var ProfileService */
+    private $profileService;
+
+    /** @var ApplicationConfig */
+    private $appConfig;
+
+    /** @var Twig_Environment */
+    private $twig;
+
+    /** @var string */
+    private $outputDir;
+
+    /**
+     * @param ProfileService
+     * @param ReportbookService
+     * @param ApplicationConfig
+     */
+    public function __construct(ProfileService $profileService, ReportbookService $reportService, ApplicationConfig $appConfig)
+    {
+        $this->appConfig = $appConfig;
+        $this->reportService = $reportService;
+        $this->profileService = $profileService;
+        $this->mpdf = new \mPDF();
+
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . $this->appConfig->printerTemplates);
+        $this->twig = new \Twig_Environment($loader);
+
+        $this->outputDir = $this->appConfig->printerOutput;
+
+        if (getenv('APPLICATION_ENV') === 'test') {
+            $this->outputDir = __DIR__ . '/../..' . $this->appConfig->printerOutput;
+
+            if (!file_exists($this->outputDir)) {
+                mkdir($this->outputDir);
+            }
+        }
+    }
+
+    /**
+     * @param string $userId
+     * @param string $trainerTitle
+     * @param string $trainerForename
+     * @param string $trainerSurname
+     * @param string $companyStreet
+     * @param string $companyCity
+     */
+    public function printCover(string $userId, string $trainerTitle, string $trainerForename, string $trainerSurname, string $companyStreet, string $companyCity, bool $printWholeReportbook = false)
+    {
+      $profile = $this->profileService->findProfileByUserId($userId);
+
+      $template = $this->twig->load('Cover.html');
+
+      $variables = [
+          'trainerTitle' => $trainerTitle,
+          'traineeForename' => $profile->forename(),
+          'traineeSurname' => $profile->surname(),
+          'trainerForename' => $trainerForename,
+          'trainerSurname' => $trainerSurname,
+          'companyName' => $profile->company(),
+          'companyStreet' => $companyStreet,
+          'companyCity' => $companyCity
+      ];
+
+      $this->mpdf->WriteHTML($template->render($variables));
+
+      if ($printWholeReportbook) {
+          $this->mpdf->AddPage();
+      } else {
+          $this->mpdf->Output($this->outputDir . '/Deckblatt.pdf','F');
+      }
+    }
+
+    /**
+    * @param string $userId
+    * @param string $startMonth
+    * @param string $startYear
+    * @param string $endMonth
+    * @param string $endYear
+    */
+    public function printReports(string $userId, string $startMonth, string $startYear, string $endMonth, string $endYear, bool $printWholeReportbook = false)
+    {
+        $profile = $this->profileService->findProfileByUserId($userId);
+        $reports = $this->reportService->findByTraineeId($userId);
+
+        if (!$printWholeReportbook) {
+            $reports = $this->getReportsForPeriod($reports, $startMonth, $startYear, $endMonth, $endYear);
+        }
+        $weekInfo = $this->createArrayForStartAndEndOfWeek($reports);
+
+
+        $maxLinesPerSite = 18;
+        $reportsPerSite = [];
+        $reportsPerSite[] = [];
+        $site = 0;
+        $reportNumber = 0;
+        $lines = 0;
+
+        foreach ($reports as $run => $report) {
+            $currentLines = $this->countLines($report->content());
+
+            if ($lines + $currentLines <= $maxLinesPerSite) {
+                $lines += $currentLines;
+                $reportNumber++;
+            } else {
+                $lines = $currentLines;
+                $site++;
+                $reportNumber = 0;
+            }
+
+            if (!isset($reportsPerSite[$site][$reportNumber])) {
+                $reportsPerSite[$site][$reportNumber] = [];
+            }
+
+            $reportsPerSite[$site][$reportNumber] = [
+                'report' => $report,
+                'weekInfo' => ['start' => $weekInfo[$run][0], 'end' => $weekInfo[$run][1]]
+            ];
+        }
+
+        $template = $this->twig->load('Report.html');
+
+        foreach ($reportsPerSite as $run => $site) {
+
+            $variables = [
+                'traineeForename' => $profile->forename(),
+                'traineeSurname' => $profile->surname(),
+                'jobTitle' => $profile->jobTitle(),
+                'site' => $site
+            ];
+
+
+            if (count($reportsPerSite) - 1 !== $run) {
+                $this->mpdf->WriteHTML($template->render($variables));
+                $this->mpdf->AddPage();
+            } else {
+                $this->mpdf->WriteHTML($template->render($variables));
+            }
+        }
+        if (!$printWholeReportbook) {
+            $this->mpdf->Output($this->outputDir . "/Berichte.pdf",'F');
+        }
+    }
+
+    /**
+     * @param string $userId
+     * @param string $trainerTitle
+     * @param string $trainerForename
+     * @param string $trainerSurname
+     * @param string $companyStreet
+     * @param string $companyCity
+     */
+    public function printReportbook(string $userId, string $trainerTitle, string $trainerForename, string $trainerSurname, string $companyStreet, string $companyCity)
+    {
+        $this->printCover($userId, $trainerTitle, $trainerForename, $trainerSurname, $companyStreet, $companyCity, true);
+        $this->printReports($userId, '', '', '', '', true);
+        $this->mpdf->Output($this->outputDir . "/Berichtsheft.pdf",'F');
+    }
+
+    /**
+     * @param string $text
+     * @return int
+     */
+    public function countLines(string $text): int
+    {
+        $li = substr_count($text, "</li>");
+        $br = substr_count($text, "<br>");
+        $nl = substr_count($text, "\r\n");
+        $p = substr_count($text, "</p>");
+
+        return $li + $br + $nl + $p;
+    }
+
+    /**
+     * @param array $reports
+     * @return array
+     */
+    private function createArrayForStartAndEndOfWeek(array $reports): array
+    {
+        $weekInfoArr = [];
+        foreach ($reports as $key => $report) {
+            $weekInfo = $this->getStartAndEndDate($report->calendarWeek(), $report->calendarYear());
+            $start = date("{$weekInfo[2]}.{$weekInfo[1]}.{$weekInfo[0]}");
+            $end = date("{$weekInfo[5]}.{$weekInfo[4]}.{$weekInfo[3]}");
+            $weekInfoArr[$key] = [
+                $start,
+                $end
+            ];
+        }
+
+        return $weekInfoArr;
+    }
+
+    /**
+     * @param array $reports
+     * @param string $startMonth
+     * @param string $startYear
+     * @param string $endMonth
+     * @param string $endYear
+     * @return array
+     */
+    private function getReportsForPeriod(array $reports, string $startMonth, string $startYear, string $endMonth, string $endYear): array
+    {
+        $matchingReports = [];
+        foreach ($reports as $report) {
+            $reportMonthAndYear = $this->getStartAndEndDate($report->calendarWeek(), $report->calendarYear());
+            $reportMonth = $reportMonthAndYear[1];
+            $reportYear = $reportMonthAndYear[0];
+
+            if ($reportYear >= $startYear && $reportYear <= $endYear) {
+                if ($reportMonth >= $startMonth && $reportMonth <= $endMonth) {
+                    $matchingReports[] = $report;
+                }
+            }
+        }
+        return $matchingReports;
+    }
+
+    /**
+     * @param int $week
+     * @param int $year
+     * @return array
+     */
+    private function getStartAndEndDate(int $week, int $year): array
+    {
+        $date = new \DateTime();
+
+        $date->setISODate($year, $week);
+
+        $startYear = intVal($date->format('Y'));
+        $startMonth = intVal($date->format('m'));
+        $startDay = intVal($date->format('d'));
+
+        $date->modify('+6 days');
+
+        $endYear = intVal($date->format('Y'));
+        $endMonth = intVal($date->format('m'));
+        $endDay = intVal($date->format('d'));
+
+        return [
+            $startYear,
+            $startMonth,
+            $startDay,
+            $endYear,
+            $endMonth,
+            $endDay
+        ];
+    }
+}
---
+++ tests/Printer/PrintServiceTest.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Jimdo\Reports\Printer;
+
+use PHPUnit\Framework\TestCase;
+use Jimdo\Reports\Profile\Profile;
+use Jimdo\Reports\Reportbook\Report;
+use Jimdo\Reports\Reportbook\TraineeId;
+use Jimdo\Reports\Web\ApplicationConfig;
+
+class PrintServiceTest extends TestCase
+{
+    /** @var PDO */
+    private $dbHandler;
+
+    /** @var ProfileService */
+    private $profileService;
+
+    /** @var ReportbookService */
+    private $reportService;
+
+    /** @var PrintService */
+    private $printService;
+
+    /** @var pdfParser */
+    private $pdfParser;
+
+    /** @var ApplicationConfig */
+    private $appConfig;
+
+    /** @var userId */
+    private $userId;
+
+    /** @var string */
+    private $outputDir;
+
+    protected function setUp()
+    {
+        $this->appConfig = new ApplicationConfig(__DIR__ . '/../../config.yml');
+        $this->userId = uniqid();
+
+        $profile = new Profile($this->userId, 'Max', 'Mustermann');
+        $profileService = \Mockery::mock('Jimdo\Reports\Profile\ProfileService');
+        $profileService->shouldReceive('createProfile')->andReturn($profile);
+        $profileService->shouldReceive('findProfileByUserId')->andReturn($profile);
+
+        $reports = [];
+        $content = "<ul><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li></ul>";
+        for ($i=1; $i < 20; $i++) {
+            $reports[] = new Report(new TraineeId($this->userId), $content, date('d-m-Y'), $i, '2017', uniqid(), 'SCHOOL');
+        }
+
+        $reportService =  \Mockery::mock('Jimdo\Reports\Reportbook\ReportbookService');
+        $reportService->shouldReceive('findByTraineeId')->andReturn($reports);
+
+        $this->profileService = $profileService;
+        $this->reportService = $reportService;
+
+        $this->pdfParser = new \Smalot\PdfParser\Parser();
+
+        $this->printService = new PrintService($this->profileService, $this->reportService, $this->appConfig);
+        $this->outputDir = __DIR__ . '/../..' . $this->appConfig->printerOutput;
+    }
+
+    protected function tearDown()
+    {
+        $this->deleteRecursive($this->outputDir);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldPrintCover()
+    {
+        $profile = $this->profileService->createProfile($this->userId, 'Max', 'Mustermann');
+
+        $this->printService->printCover($this->userId, 'Herr', 'Hauke', 'Stange', 'Stresemannstraße 375', '22761 Hamburg');
+
+        $pdf = $this->pdfParser->parseFile($this->outputDir . '/Deckblatt.pdf');
+
+        $pages  = $pdf->getPages();
+
+        $this->assertEquals(0, strpos($pages[0]->getText(), 'Berichtsheft'));
+        $this->assertTrue(file_exists($this->outputDir . '/Deckblatt.pdf'));
+        $this->assertCount(1, $pages);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldPrintReports()
+    {
+        $profile = $this->profileService->createProfile($this->userId, 'Max', 'Mustermann');
+        $profile->editJobTitle('Fachinformatiker Anwendungsentwicklung');
+
+        $this->printService->printReports($this->userId, '2', '2017', '3', '2017');
+
+        $pdf = $this->pdfParser->parseFile($this->outputDir . '/Berichte.pdf');
+
+        $pages  = $pdf->getPages();
+
+        $this->assertEquals(212, strpos($pages[0]->getText(), '9'));
+        $this->assertCount(4, $pages);
+        $this->assertTrue(file_exists($this->outputDir . '/Berichte.pdf'));
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldPrintReportbook()
+    {
+        $profile = $this->profileService->createProfile($this->userId, 'Hase', 'Igel');
+        $profile->editJobTitle('Fachinformatiker Anwendungsentwicklung');
+
+        $this->printService->printReportbook($this->userId, 'Herr', 'Hase', 'Igel', 'Stresemannstraße 375', '22761 Hamburg');
+
+        $pdf = $this->pdfParser->parseFile($this->outputDir . '/Berichtsheft.pdf');
+
+        $pages  = $pdf->getPages();
+
+        $this->assertTrue(file_exists($this->outputDir . '/Berichtsheft.pdf'));
+        $this->assertCount(11, $pages);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldCountLines()
+    {
+        $text = "<ul><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li></ul>";
+        $countedLines = $this->printService->countLines($text);
+        $this->assertEquals(7, $countedLines);
+
+        $text = "<p>1</p><p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7<br>8</p>";
+        $countedLines = $this->printService->countLines($text);
+        $this->assertEquals(8, $countedLines);
+
+        $text = "<ol><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li></ol>";
+        $countedLines = $this->printService->countLines($text);
+        $this->assertEquals(8, $countedLines);
+    }
+
+    private function deleteRecursive($input)
+    {
+        if (is_file($input)) {
+            unlink($input);
+            return;
+        }
+
+        if (is_dir($input)) {
+            foreach (scandir($input) as $file) {
+                if ($file === '.' || $file === '..') {
+                    continue;
+                }
+
+                $file = join('/', [$input, $file]);
+                if (is_file($file)) {
+                    unlink($file);
+                    continue;
+                }
+
+                $this->deleteRecursive($file);
+            }
+        }
+    }
+}

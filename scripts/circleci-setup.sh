1813508971e3a66e60619b815501f1d1396c75f2
Tom Stich <tom.stich@jimdo.com>
2017-03-31 14:47:52 +0200

    Modify circleci setup

--- scripts/circleci-setup.sh
+++ scripts/circleci-setup.sh
@@ -4,14 +4,4 @@ set -e
 
 pwd 
 
-docker run --name reportbook-mongodb \
-    -v reportbook-data:/data/db \
-    -v /home/ubuntu/reportbook/scripts:/scripts \
-    -p $MONGO_PORT:27017 \
-    -d mongo --auth
-
-# let the MongoDB server start up...
-echo "Waiting 2 seconds for the MongoDB server..."
-sleep 2
-
-docker exec reportbook-mongodb /scripts/setup-mongo-server.sh
+/scripts/setup-mongo-server.sh

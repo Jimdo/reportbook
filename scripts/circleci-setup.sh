4111bd566c90dd6bd507086a70cfe185d827af45
Tom Stich <tom.stich@jimdo.com>
2017-03-31 13:56:29 +0200

    Add circle ci setup

---
+++ scripts/circleci-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+pwd 
+
+docker run --name reportbook-mongodb \
+    -v reportbook-data:/data/db \
+    -v ./scripts:/scripts \
+    -p 27017:27017 \
+    -d mongo --auth
+
+# let the MongoDB server start up...
+echo "Waiting 2 seconds for the MongoDB server..."
+sleep 2
+
+docker exec reportbook-mongodb /scripts/setup-mongo-server.sh

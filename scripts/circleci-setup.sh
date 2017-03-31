6a449ad82d4cf1eab7f64580857870b121d3cc4b
Tom Stich <tom.stich@jimdo.com>
2017-03-31 15:42:34 +0200

    Modify circleci setup with database setups

--- scripts/circleci-setup.sh
+++ scripts/circleci-setup.sh
@@ -2,41 +2,10 @@
 
 set -e
 
-pwd 
+## Setup mongo user and collections
+./scripts/circleci-mongo-setup.sh
 
-if [ -z ${MONGO_ADMIN_PASSWORD+x} ]; then
-  MONGO_ADMIN_PASSWORD=geheim
-fi
+## Setup mysql user and database
+./scripts/circleci-mysql-setup.sh
 
-if [ -z ${MONGO_USERNAME+x} ]; then
-  MONGO_USERNAME=reportbook
-fi
-
-if [ -z ${MONGO_PASSWORD+x} ]; then
-  MONGO_PASSWORD=geheim
-fi
-
-if [ -z ${MONGO_DATABASE+x} ]; then
-  MONGO_DATABASE=reportbook
-fi
-
-# Create admin user
-sed "s/PASSWORD/$MONGO_ADMIN_PASSWORD/g" ./scripts/mongo/create-admin-user.js | mongo admin
-
-# Create dev user
-sed "s/USERNAME/$MONGO_USERNAME-dev/g" ./scripts/mongo/create-user.js \
-    | sed "s/PASSWORD/$MONGO_PASSWORD/g" \
-    | sed "s/DATABASE/${MONGO_DATABASE}_dev/g" \
-    | mongo admin -u admin -p $MONGO_ADMIN_PASSWORD
-
-# Create test user
-sed "s/USERNAME/$MONGO_USERNAME-test/g" ./scripts/mongo/create-user.js \
-    | sed "s/PASSWORD/$MONGO_PASSWORD/g" \
-    | sed "s/DATABASE/${MONGO_DATABASE}_test/g" \
-    | mongo admin -u admin -p $MONGO_ADMIN_PASSWORD
-
-# Create collections
-./scripts/mongo-collections.sh
-
-# Create unique indices
-./scripts/mongo-indices.sh
+echo "extension=mongodb.so" >> /opt/circleci/php/7.0.4/etc/php.ini

da69122fca097e9a0132a560050d8214dc6f1c71
Tom Stich <tom.stich@jimdo.com>
2017-04-04 15:55:46 +0200

    Try to fix circle ci

--- scripts/circleci-setup.sh
+++ scripts/circleci-setup.sh
@@ -9,15 +9,6 @@ curl http://chromedriver.storage.googleapis.com/2.23/chromedriver_linux64.zip | 
 
 chmod +x chromedriver
 
-# Update Google Chrome.
-wget -q -O https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb stable main" >> /etc/apt/sources.list.d/google.list'
-
-sudo apt-get update
-
-sudo apt-get --only-upgrade install google-chrome-stable
-
 ## Setup mongo user and collections
 ./scripts/circleci-mongo-setup.sh
 

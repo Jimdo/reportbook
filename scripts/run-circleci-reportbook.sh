d2be302ec19877633cf79edf3108d6f94c207884
Tom Stich <tom.stich@jimdo.com>
2017-04-06 14:54:06 +0200

    Fix tests in circle ci

--- scripts/run-circleci-reportbook.sh
+++ scripts/run-circleci-reportbook.sh
@@ -7,10 +7,6 @@ then
     source .env
 fi
 
-if [ -z ${REPORTBOOK_IP+x} ]; then
-  export REPORTBOOK_IP=$(docker-machine ip)
-fi
-
 docker run -d \
     -p 80:80 \
     -e APPLICATION_ENV=$APPLICATION_ENV \
@@ -21,6 +17,5 @@ docker run -d \
     -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
     -e MAILGUN_DOMAIN=$MAILGUN_DOMAIN \
     -e MAILGUN_KEY=$MAILGUN_KEY \
-    -e REPORTBOOK_IP=$REPORTBOOK_IP \
     -v $(pwd)/:/var/www/ \
     jimdo/reportbook
--- scripts/run-tests-circleci.sh
+++ scripts/run-tests-circleci.sh
@@ -8,6 +8,10 @@ if [ -z ${MYSQL_HOST+x} ]; then
   export MYSQL_HOST=127.0.0.1
 fi
 
+if [ -z ${REPORTBOOK_IP+x} ]; then
+  export REPORTBOOK_IP=$(docker-machine ip)
+fi
+
 export APPLICATION_ENV=test
 
 ./scripts/phpunit

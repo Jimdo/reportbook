28bba8d3de2a8e19f9d04c16f7d5e4e8227e9f94
Tom Stich <tom.stich@jimdo.com>
2017-04-06 14:01:43 +0200

    Rename script

---
+++ scripts/run-circleci-reportbook.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+APPLICATION_ENV=dev
+
+if [ -e .env ]
+then
+    source .env
+fi
+
+export REPORTBOOK_IP=$(docker-machine ip)
+
+docker run -d --rm \
+    -p 90:80 \
+    -e APPLICATION_ENV=$APPLICATION_ENV \
+    -e MONGO_HOST=$MONGO_HOST \
+    -e MYSQL_HOST=$MYSQL_HOST \
+    -e MYSQL_DATABASE=$MYSQL_DATABASE \
+    -e MYSQL_USER=$MYSQL_USER \
+    -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+    -e MAILGUN_DOMAIN=$MAILGUN_DOMAIN \
+    -e MAILGUN_KEY=$MAILGUN_KEY \
+    -e REPORTBOOK_IP=$REPORTBOOK_IP \
+    -v $(pwd)/:/var/www/ \
+    jimdo/reportbook

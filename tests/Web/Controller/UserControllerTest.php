ce6d434445219fc82183a435e2d8c2e3eff72e24
Tom Stich <tom.stich@jimdo.com>
2017-04-06 15:14:57 +0200

    Try to look if the setup is running

--- tests/Web/Controller/UserControllerTest.php
+++ tests/Web/Controller/UserControllerTest.php
@@ -47,55 +47,7 @@ class UserControllerTest extends TestCase
      */
     public function itShouldTestPageTitle()
     {
-        $this->webDriver->get("{$this->url}/user");
-        $this->assertContains('Berichtsheft', $this->webDriver->getTitle());
-    }
-    
-    /**
-     * @test
-     */
-    public function itShouldUploadImage()
-    {
-        $serializer = new Serializer();
-
-        $repositoryFactory = new RepositoryFactory($this->appConfig, $serializer);
-        
-        $profileRepository = $repositoryFactory->createProfileRepository();
-        $userRepository = $repositoryFactory->createUserRepository();
-
-        // Login process
-        $this->webDriver->get("{$this->url}/user");
-
-        $username = $this->webDriver->findElement(\WebDriverBy::id('identifier'));
-        $username->click();
-        $this->webDriver->getKeyboard()->sendKeys('admin');
-
-        $password = $this->webDriver->findElement(\WebDriverBy::id('password'));
-        $password->click();
-        $this->webDriver->getKeyboard()->sendKeys('Adminadmin123');
-
-        $this->webDriver->getKeyboard()->pressKey(\WebDriverKeys::ENTER);
-
-        // Upload Picture process
-        $linkToProfile = $this->webDriver->findElement(\WebDriverBy::partialLinkText("Profil"));
-        $linkToProfile->click();
-
-        $editPicture = $this->webDriver->findElement(
-            \WebDriverBy::cssSelector('div.profile-picture > a.glyphicon')
-        );    
-        $editPicture->click();
-
-        $fileInput = $this->webDriver->findElement(\WebDriverBy::id('fileToUpload'));
-
-        $fileInput->setFileDetector(new \LocalFileDetector());
-
-        $fileInput->sendKeys('/var/www/tests/Web/Controller/test-picture.png')->submit();
-        
-        $user = $userRepository->findUserByUsername('admin');
-        $profile = $profileRepository->findProfileByUserId($user->id());
-        $baseOfProfilePicture = $profileRepository->findProfileByUserId($user->id())->image();
-        $baseOfFile = base64_encode(file_get_contents('/var/www/tests/Web/Controller/test-picture.png'));
-        
-        $this->assertEquals($baseOfFile, $baseOfProfilePicture);
+        $this->webDriver->get("https://github.com");
+        $this->assertContains('GitHub', $this->webDriver->getTitle());
     }
 }

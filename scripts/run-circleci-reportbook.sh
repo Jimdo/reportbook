bbf107f6ecc185f26b8f3934ef70c7d7f9ac7a4f
Tom Stich <tom.stich@jimdo.com>
2017-04-07 11:41:11 +0200

    Change IP address

--- scripts/run-circleci-reportbook.sh
+++ scripts/run-circleci-reportbook.sh
@@ -7,16 +7,17 @@ then
     source .env
 fi
 
-docker run -d \
+export DOCKER_HOST_IP=$(route -n | awk '/UG[ \t]/{print $2}')
+
+docker run --net="host" -d \
     -p 80:80 \
     -e APPLICATION_ENV=$APPLICATION_ENV \
-    -e MONGO_HOST=$MONGO_HOST \
-    -e MYSQL_HOST=$MYSQL_HOST \
+    -e MONGO_HOST=$DOCKER_HOST_IP \
+    -e MYSQL_HOST=$DOCKER_HOST_IP \
     -e MYSQL_DATABASE=$MYSQL_DATABASE \
     -e MYSQL_USER=$MYSQL_USER \
     -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
     -e MAILGUN_DOMAIN=$MAILGUN_DOMAIN \
     -e MAILGUN_KEY=$MAILGUN_KEY \
     -v $(pwd)/:/var/www/ \
-    --add-host="localhost:10.0.2.2" \
     jimdo/reportbook

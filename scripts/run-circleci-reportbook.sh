cfad8ff70bea711baef081b9960713f53cf5f7f2
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-07 12:37:34 +0200

    Remove debug

--- scripts/run-circleci-reportbook.sh
+++ scripts/run-circleci-reportbook.sh
@@ -7,13 +7,11 @@ then
     source .env
 fi
 
-export DOCKER_HOST_IP=$(route -n | awk '/UG[ \t]/{print $2}')
-
-docker run --net="host" -d \
+docker run -d \
     -p 80:80 \
     -e APPLICATION_ENV=$APPLICATION_ENV \
-    -e MONGO_HOST=$DOCKER_HOST_IP \
-    -e MYSQL_HOST=$DOCKER_HOST_IP \
+    -e MONGO_HOST=$MONGO_HOST \
+    -e MYSQL_HOST=$MYSQL_HOST \
     -e MYSQL_DATABASE=$MYSQL_DATABASE \
     -e MYSQL_USER=$MYSQL_USER \
     -e MYSQL_PASSWORD=$MYSQL_PASSWORD \

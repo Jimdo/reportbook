3678efd72e1d6be7231bf7cba7e950cac801c629
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-11 13:52:02 +0200

    Remove link in reportbook container

--- scripts/run-circleci-reportbook.sh
+++ scripts/run-circleci-reportbook.sh
@@ -7,7 +7,7 @@ then
     source .env
 fi
 
-docker run -d --link selenium-hub:hub \
+docker run -d \
     -p 27017:27017 \
     -e APPLICATION_ENV=$APPLICATION_ENV \
     -e MONGO_HOST=$MONGO_HOST \

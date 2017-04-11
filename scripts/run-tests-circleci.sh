2bb1f01e8194d988971392c02e621f51d860c2be
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-11 16:04:57 +0200

    Remove superfluous Variables

--- scripts/run-tests-circleci.sh
+++ scripts/run-tests-circleci.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-if [ -z ${MONGO_HOST+x} ]; then
-  export MONGO_HOST=$(docker-machine ip)
-fi
-
-if [ -z ${MYSQL_HOST+x} ]; then
-  export MYSQL_HOST=$(docker-machine ip)
-fi
-
-if [ -z ${REPORTBOOK_IP+x} ]; then
-  export REPORTBOOK_IP=$(docker-machine ip)
-fi
-
 export APPLICATION_ENV=test
 
 export REPORTBOOK_IP=$(ip addr show docker0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)

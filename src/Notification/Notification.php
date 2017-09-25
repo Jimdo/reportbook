004948e59be1ac7cb04f27e051c8d58eeda32745
Tom Stich <tom.stich@jimdo.com>
2017-09-25 10:51:25 +0200

    Add Notification

---
+++ src/Notification/Notification.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jimdo\Reports\Notification;
+
+class Notification
+{
+    const STATUS_NEW = 'NEW';
+
+    /** @var string */
+    private $title;
+
+    /** @var string */
+    private $description;
+
+    /** @var string */
+    private $status;
+
+    /**
+     * @param string $title
+     * @param string $description
+     */
+    public function __construct(String $title, String $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+
+        $this->status = self::STATUS_NEW;
+    }
+
+    public function title(): String
+    {
+        return $this->title;
+    }
+
+    public function description(): String
+    {
+        return $this->description;
+    }
+
+    public function status(): String
+    {
+        return $this->status;
+    }
+}

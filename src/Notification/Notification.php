c972c2b3de7fd07faaa28444acf7e687ac81bdb2
Tom Stich <tom.stich@jimdo.com>
2017-09-25 11:13:27 +0200

    Implement Notification (#507)
    * Add Test for NotificationTest
    
    * Add Notification
    
    * Add new constant
    
    * Add return types
    
    * Add status seen
    
    * Add return description

---
+++ src/Notification/Notification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Jimdo\Reports\Notification;
+
+class Notification
+{
+    const STATUS_NEW = 'NEW';
+    const STATUS_SEEN = 'SEEN';
+
+    /** @var string */
+    private $title;
+
+    /** @var string */
+    private $description;
+
+    /** @var string */
+    private $status;
+
+    /**
+     * @param string $title
+     * @param string $description
+     */
+    public function __construct(String $title, String $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+
+        $this->status = self::STATUS_NEW;
+    }
+
+    /**
+     * @return string
+     */
+    public function title(): String
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function description(): String
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function status(): String
+    {
+         return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function seen()
+    {
+        $this->status = self::STATUS_SEEN;
+    }
+}
---
+++ tests/functional/Notification/NotificationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Jimdo\Reports\Notification;
+
+use PHPUnit\Framework\TestCase;
+use Jimdo\Reports\Web\ApplicationConfig as ApplicationConfig;
+
+
+class NotificationTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldHaveVariables()
+    {
+        $title = 'Title';
+        $description =  'A beautiful description';
+
+        $notification = new Notification($title, $description);
+
+        $this->assertEquals($title, $notification->title());
+        $this->assertEquals($description, $notification->description());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveStatusNewAfterCreation()
+    {
+        $title = 'title';
+        $description = 'description';
+
+        $notification = new Notification($title, $description);
+
+        $this->assertEquals(Notification::STATUS_NEW, $notification->status());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveStatusSeen()
+    {
+        $title = 'title';
+        $description = 'description';
+
+        $notification = new Notification($title, $description);
+
+        $notification->seen();
+
+        $this->assertEquals(Notification::STATUS_SEEN, $notification->status());
+    }
+}

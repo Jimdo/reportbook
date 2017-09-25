3b5f0137df65e9616a0d54296259302177f8ac2c
Tom Stich <tom.stich@jimdo.com>
2017-09-25 11:55:10 +0200

    Add timestamp and reportId to Notification (#508)
    * Add reportId to Notification
    
    * Add time to Notification

--- src/Notification/Notification.php
+++ src/Notification/Notification.php
@@ -14,18 +14,27 @@ class Notification
     private $description;
 
     /** @var string */
+    private $reportId;
+
+    /** @var string */
     private $status;
+
+    /** @var int */
+    private $time;
 
     /**
      * @param string $title
      * @param string $description
+     * @param string $reportId
      */
-    public function __construct(String $title, String $description)
+    public function __construct(String $title, String $description, String $reportId)
     {
         $this->title = $title;
         $this->description = $description;
+        $this->reportId = $reportId;
 
         $this->status = self::STATUS_NEW;
+        $this->time = time();
     }
 
     /**
@@ -47,9 +56,25 @@ class Notification
     /**
      * @return string
      */
+    public function reportId(): String
+    {
+        return $this->reportId;
+    }
+
+    /**
+     * @return string
+     */
     public function status(): String
     {
-         return $this->status;
+        return $this->status;
+    }
+
+    /**
+    * @return int
+    */
+    public function time(): int
+    {
+        return $this->time;
     }
 
     /**
--- tests/functional/Notification/NotificationTest.php
+++ tests/functional/Notification/NotificationTest.php
@@ -15,38 +15,56 @@ class NotificationTest extends TestCase
     {
         $title = 'Title';
         $description =  'A beautiful description';
+        $reportId =  uniqid();
 
-        $notification = new Notification($title, $description);
+        $notification = new Notification($title, $description, $reportId);
 
         $this->assertEquals($title, $notification->title());
         $this->assertEquals($description, $notification->description());
+        $this->assertEquals($reportId, $notification->reportId());
     }
 
     /**
-     * @test
-     */
+    * @test
+    */
     public function itShouldHaveStatusNewAfterCreation()
     {
         $title = 'title';
         $description = 'description';
+        $reportId =  uniqid();
 
-        $notification = new Notification($title, $description);
+        $notification = new Notification($title, $description, $reportId);
 
         $this->assertEquals(Notification::STATUS_NEW, $notification->status());
     }
 
     /**
-     * @test
-     */
+    * @test
+    */
     public function itShouldHaveStatusSeen()
     {
         $title = 'title';
         $description = 'description';
+        $reportId =  uniqid();
 
-        $notification = new Notification($title, $description);
+        $notification = new Notification($title, $description, $reportId);
 
         $notification->seen();
 
         $this->assertEquals(Notification::STATUS_SEEN, $notification->status());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveTimestamp()
+    {
+        $title = 'title';
+        $description = 'description';
+        $reportId =  uniqid();
+
+        $notification = new Notification($title, $description, $reportId);
+
+        $this->assertInternalType("int", $notification->time());
     }
 }

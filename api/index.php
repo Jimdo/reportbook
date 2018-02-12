31ff7bcc3c39908280d3e648f5e5abfd112f0aa6
Tom Stich <tom.stich@jimdo.com>
2018-02-12 11:35:04 +0100

    Remove username response in login

--- api/index.php
+++ api/index.php
@@ -65,7 +65,7 @@ $app->post('/login', function (Silex\Application $app, Request $request) use ($a
         $user = $appService->findUserByUsername($username);
         $_SESSION['userId'] = $user->id();
 
-        return new Response(json_encode(['username' => $user->username()]), 200);
+        return new Response(json_encode(['status' => 'ok']), 200);
     } else {
         $_SESSION['userId'] = '';
         return new Response(json_encode(['status' => 'unauthorized']), 401);

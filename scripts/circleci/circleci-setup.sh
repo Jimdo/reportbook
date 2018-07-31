75403a18d2b6534ac8504210715f02162fa1141b
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2018-07-31 12:04:07 +0200

    Move circleci configurations to own folder

--- scripts/circleci/circleci-setup.sh
+++ scripts/circleci/circleci-setup.sh
@@ -6,7 +6,7 @@ set -e
 echo "extension=mongodb.so" | sudo tee /usr/local/etc/php/php.ini > /dev/null
 
 ## Setup mongo user and collections
-./scripts/circleci-mongo-setup.sh
+./scripts/circleci/circleci-mongo-setup.sh
 
 ## Setup mysql user and database
-./scripts/circleci-mysql-setup.sh
+./scripts/circleci/circleci-mysql-setup.sh

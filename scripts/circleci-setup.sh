bd81f2cb543c301be49c0af98c425c42504bfd36
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2018-07-31 10:54:25 +0200

    Add mongodb extension into right php ini direcotry

--- scripts/circleci-setup.sh
+++ scripts/circleci-setup.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-echo "extension=mongodb.so" >> /opt/circleci/php/$(php -r 'echo phpversion();')/etc/php.ini
+# echo "extension=mongodb.so" >> /opt/circleci/php/$(php -r 'echo phpversion();')/etc/php.ini
+echo "extension=mongodb.so" | sudo tee /usr/local/etc/php/php.ini > /dev/null
 
 ## Setup mongo user and collections
 ./scripts/circleci-mongo-setup.sh

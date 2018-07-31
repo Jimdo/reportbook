0cc0a38db6ed458c714a8fed188fa275cfca07a7
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2018-07-31 10:50:09 +0200

    WIP

--- scripts/circleci-setup.sh
+++ scripts/circleci-setup.sh
@@ -2,10 +2,10 @@
 
 set -e
 
+echo "extension=mongodb.so" >> /opt/circleci/php/$(php -r 'echo phpversion();')/etc/php.ini
+
 ## Setup mongo user and collections
 ./scripts/circleci-mongo-setup.sh
 
 ## Setup mysql user and database
 ./scripts/circleci-mysql-setup.sh
-
-echo "extension=mongodb.so" >> /opt/circleci/php/$(php -r 'echo phpversion();')/etc/php.ini

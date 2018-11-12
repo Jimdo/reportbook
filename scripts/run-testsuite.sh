804c01689fe6769755cd1ecc6ef07c7e81611c1e
Tom Stich <tom.stich@jimdo.com>
2018-11-12 13:04:43 +0100

    WIP

--- scripts/run-testsuite.sh
+++ scripts/run-testsuite.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 if [ -z ${MONGO_HOST+x} ]; then
-  export MONGO_HOST=$(docker-machine ip)
+  export MONGO_HOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' reportbook_reportbook_1)
 fi
 
 if [ -z ${MYSQL_HOST+x} ]; then
-  export MYSQL_HOST=$(docker-machine ip)
+  export MYSQL_HOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' reportbook_reportbook_1)
 fi
 
 if [ -z ${SELENIUM_IP+x} ]; then
-  export SELENIUM_IP=$(docker-machine ip)
+  export SELENIUM_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' reportbook_reportbook_1)
 fi
 
 export APPLICATION_ENV=test
 
-export REPORTBOOK_IP=$(docker-machine ip)
+export REPORTBOOK_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' reportbook_reportbook_1)
 
 export DOCKER_HOST_IP=$(docker-machine inspect --format '{{ .Driver.HostOnlyCIDR}}' | awk -F/ '{print $1}')
 
